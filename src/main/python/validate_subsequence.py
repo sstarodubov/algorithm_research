bb61bd560c1e21f402d8a442b8269d0fdee02e20
haxul <s.s.starodubov@yandex.ru>
2021-02-02 20:04:59 +0400

    validate

---
+++ src/main/python/validate_subsequence.py
@@ -0,0 +1,24 @@
+# Проверить является ли need_validate_sub
+# валидной последовательностью init_sub
+
+init_sub = [5, 1, 22, 25, 6, -1, 8, 10]
+need_validate_sub = [1, 6, -1, 10]
+
+
+def is_valid_sub(source_sub, valid_sub):
+    pointer = 0
+    for num in source_sub:
+        if valid_sub[pointer] == num:
+            pointer = pointer + 1
+
+    return pointer == len(valid_sub)
+
+
+def is_valid_sub_rec(source_sub, valid_sub, pointer=0):
+    if not source_sub:
+        return pointer == len(valid_sub)
+    head, *tail = source_sub
+    if head == valid_sub[pointer]:
+        pointer = pointer + 1
+    return is_valid_sub_rec(tail, valid_sub, pointer)
+
