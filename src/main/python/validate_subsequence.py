20d09839933551384995bd14945c439375d894bd
haxul <s.s.starodubov@yandex.ru>
2021-02-02 20:12:45 +0400

    validate

--- src/main/python/validate_subsequence.py
+++ src/main/python/validate_subsequence.py
@@ -10,15 +10,18 @@ def is_valid_sub(source_sub, valid_sub):
     for num in source_sub:
         if valid_sub[pointer] == num:
             pointer = pointer + 1
+        if pointer == len(valid_sub):
+            return True
 
-    return pointer == len(valid_sub)
+    return False
 
 
 def is_valid_sub_rec(source_sub, valid_sub, pointer=0):
+    if pointer == len(valid_sub):
+        return True
     if not source_sub:
-        return pointer == len(valid_sub)
+        return False
     head, *tail = source_sub
     if head == valid_sub[pointer]:
         pointer = pointer + 1
     return is_valid_sub_rec(tail, valid_sub, pointer)
-
