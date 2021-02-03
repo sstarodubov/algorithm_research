008af88aed5fb917791b0be0af47ec1a2d717b4a
haxul <s.s.starodubov@yandex.ru>
2021-02-03 22:12:40 +0400

    validate

--- src/main/python/branch_sum.py
+++ src/main/python/branch_sum.py
@@ -2,7 +2,16 @@ from binarytree import build
 from binarytree import Node
 from typing import List
 
-nodes = [10, 5, 15, 2, 5, 13, 22, 1, None, None, None, None, 14]
+# Дано бинарное дерево
+#         _____1__
+#        /        \
+#     __2___       3
+#    /      \     / \
+#   4       _5   6   7
+#  / \     /
+# 8   9   10
+# нужно посчитать сумму каждой ветки
+nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # Building the tree
 tree: Node = build(nodes)
@@ -10,40 +19,26 @@ print('Binary tree from list :\n',
       tree)
 
 
-# O(lg n)
-def find_node_trace(node: Node, trace: List[int], cur_node_value: int):
-    trace.append(node.value)
-    if node.value == cur_node_value and node.right is None and node.left is None:
-        return trace
-    if node.value <= cur_node_value:
-        return find_node_trace(node.right, trace, cur_node_value)
-    else:
-        return find_node_trace(node.left, trace, cur_node_value)
+def sum_branches_rec(root: Node):
+    sums = []
+    calculate(node=root, sums=sums)
+    return sums
 
 
-# O(lg n)
-def find_last_leaves(node: Node, result: List[int]) -> List[int]:
-    if node is not None:
-        find_last_leaves(node.left, result)
-        if node.left is None and node.right is None:
-            result.append(node.value)
-        find_last_leaves(node.right, result)
-    return result
+def calculate(node: Node, sums: List[int], running_sum: int = 0):
+    new_running_sum = running_sum + node.val
+
+    if node.left is None and node.right is None:
+        sums.append(new_running_sum)
+        return
+    if node.left is None:
+        calculate(node.right, sums, new_running_sum)
+        return
+    if node.right is None:
+        calculate(node.left, sums, new_running_sum)
+        return
+    calculate(node.left, sums, new_running_sum)
+    calculate(node.right, sums, new_running_sum)
 
 
-
-# O(n)
-def find_sum_branch(_tree: Node, _leaves: List[int], low: int, high: int, result=[]):
-    if high == low:
-        r = sum(find_node_trace(_tree, [], _leaves[high]))
-        result.append(r)
-    else:
-        mid = int((high + low) / 2)
-        find_sum_branch(_tree, _leaves, low, mid)
-        find_sum_branch(_tree, _leaves, mid + 1, high)
-    return result
-
-
-leaves = find_last_leaves(tree, [])
-res = find_sum_branch(tree, leaves, 0, len(leaves) - 1)
-print(res)
+print(sum_branches_rec(tree))
