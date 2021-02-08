bd292710e8987b926602f5ddb21f04c1fe35d381
haxul <s.s.starodubov@yandex.ru>
2021-02-08 21:11:20 +0400

    bubble

--- src/main/python/simple_sort.py
+++ src/main/python/simple_sort.py
@@ -1,6 +1,7 @@
 arr = [8, 5, 2, 9, 5, 6, 3]
 
 
+# time O(n^2) space O(1)
 def insertion_sort(arr: [int]) -> [int]:
     for current in range(1, len(arr)):
         prev = current - 1
@@ -11,4 +12,31 @@ def insertion_sort(arr: [int]) -> [int]:
     return arr
 
 
-print(insertion_sort(arr))
+def bubble_sort(arr: [int]):
+    right_p = len(arr) - 2
+    while True:
+        cur_inx = 0
+        swap = False
+        while cur_inx <= right_p:
+            if arr[cur_inx] > arr[cur_inx + 1]:
+                arr[cur_inx], arr[cur_inx + 1] = arr[cur_inx + 1], arr[cur_inx]
+                swap = True
+            cur_inx += 1
+        right_p -= 1
+        if not swap: return arr
+
+
+def bubble_sort2(arr: [int]):
+    sorted = False
+    count = 0
+    while not sorted:
+        sorted = True
+        for n in range(len(arr) - 1 - count):
+            if arr[n] > arr[n + 1]:
+                arr[n], arr[n + 1] = arr[n + 1], arr[n]
+                sorted = False
+        count += 1
+    return arr
+
+
+print(bubble_sort2(arr))
