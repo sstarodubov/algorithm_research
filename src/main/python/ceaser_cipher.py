9ed9befdd51a18b803e785f7ce18bf1bf43faf5c
haxul <s.s.starodubov@yandex.ru>
2021-02-10 21:31:15 +0400

    bubble

--- src/main/python/ceaser_cipher.py
+++ src/main/python/ceaser_cipher.py
@@ -3,23 +3,8 @@ import string
 word = "xyz"
 key = 2
 
-alphabet = {}
-for i in range(len(string.ascii_lowercase)):
-    alphabet[string.ascii_lowercase[i]] = i
-
-alpha_list = string.ascii_lowercase
 # Time O(n) Space O(n)
-def caesar_cipher(word: str, key: int) -> str:
-    result = ""
-    for n in word:
-        cur_inx = alphabet.get(n)
-        new_inx = cur_inx + key
-        if new_inx > 25:
-            new_inx %= len(alpha_list)
-        new_letter = alpha_list[new_inx]
-        result += new_letter
-    return result
-
+# a = 97 z = 122
 def caesar_cipher2(word: str, key: int) :
     new_letters = []
     new_key = key % 26
