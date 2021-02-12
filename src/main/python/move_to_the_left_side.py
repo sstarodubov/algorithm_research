6d12b8b917f7c2c98a052d467906478d2d3a99a5
haxul <s.s.starodubov@yandex.ru>
2021-02-12 20:12:36 +0400

    bubble

--- src/main/python/move_to_the_left_side.py
+++ src/main/python/move_to_the_left_side.py
@@ -2,16 +2,29 @@
 дан массив с числами и число n
 нужно переместить все числа вправо в массиве значение которых равно n
 """
-nums = [2, 1, 2, 2, 2, 3, 4, 2]
+nums = [2, 1, 2, 3, 2, 2, 3, 4, 2]
 target = 2
 
-# Time O(n) Space (n)
+
+# Time O(n) Space (n) with order
 def move_target_to_left(numbers: [int], num: int):
     result = []
     for n in range(len(numbers) - 1, -1, -1):
         if nums[n] == num:
             result.append(nums[n])
-        else: result.insert(0, nums[n])
+        else:
+            result.insert(0, nums[n])
     return result
 
 
+# Time O(n) Space O(1)
+def move_without_order(numbers: [int], num: int):
+    left_ptr = 0
+    right_ptr = len(numbers) - 1
+    while left_ptr < right_ptr:
+        if numbers[right_ptr] == num:
+            right_ptr -= 1
+        if numbers[left_ptr] == num:
+            numbers[left_ptr], numbers[right_ptr] = numbers[right_ptr], numbers[left_ptr]
+        left_ptr += 1
+    return numbers
