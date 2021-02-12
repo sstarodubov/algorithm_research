3cd4f21f49235db0853d8d9c2c0cb15ccc4fa6b9
haxul <s.s.starodubov@yandex.ru>
2021-02-12 21:45:34 +0400

    bubble

---
+++ src/main/python/monotoic_array.py
@@ -0,0 +1,38 @@
+arr = [-1, -1, -10, -1100, -1100, -1101, -1102, -9001]
+increasing_arr = [1, 1, 3, 4, 5, 6, 7, 8]
+
+
+# time O(n) space O(1)
+def is_monotonic(ar: [int]):
+    global decreasing
+    global left, right
+
+    if ar[0] == ar[1]:
+        left = 0
+        right = 1
+        while ar[left] == ar[right]:
+            left += 1
+            right += 1
+        decreasing = ar[left] > ar[right]
+    else:
+        decreasing = ar[0] > ar[1]
+        left = 0
+        right = 1
+
+    if decreasing:
+        while right < len(ar):
+            if ar[left] < ar[right]:
+                return False
+            left += 1
+            right += 1
+        return True
+    else:
+        while right < len(ar):
+            if ar[left] > ar[right]:
+                return False
+            left += 1
+            right += 1
+        return True
+
+
+print(is_monotonic(arr))
