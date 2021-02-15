1e28744866f095cc5bac5e895460f05b4f00ecac
haxul <s.s.starodubov@yandex.ru>
2021-02-15 21:50:23 +0400

    bubble

--- src/main/python/spiral_traverse.py
+++ src/main/python/spiral_traverse.py
@@ -51,4 +51,25 @@ def spirit_traverse(arr: [[int]], vertexes: {}, from_ptr: str = "LH", x=0, y=0, 
         return spirit_traverse(arr, vertexes, from_ptr, x, y, trace)
 
 
-print(spirit_traverse(array, vertexes))
+def spiral_traverse2(array):
+    result = []
+    start_row, end_row = 0, len(array) - 1
+    start_col, end_col = 0, len(array[0]) - 1
+    while start_row <= end_row and start_col <= end_col:
+        for col in range(start_col, end_col + 1):
+            result.append(array[start_row][col])
+
+        for row in range(start_row + 1, end_row + 1):
+            result.append(array[row][end_col])
+
+        for col in reversed(range(start_col, end_col)):
+            result.append(array[end_row][col])
+
+        for row in reversed(range(start_row + 1, end_row)):
+            result.append(array[row][start_col])
+
+        start_row += 1
+        end_row -= 1
+        start_col += 1
+        end_col -= 1
+    return result
