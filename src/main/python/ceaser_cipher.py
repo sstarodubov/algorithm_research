322e6a41dbbb2d44c14e3454d6ea0a34020cddee
starodubov <s.s.starodubov@yandex.com>
2021-02-16 16:40:33 +0400

    FF

--- src/main/python/ceaser_cipher.py
+++ src/main/python/ceaser_cipher.py
@@ -1,19 +1,20 @@
-
-
 word = "xyz"
 key = 2
 
+
 # Time O(n) Space O(n)
 # a = 97 z = 122
-def caesar_cipher(word: str, key: int) :
+def caesar_cipher(word: str, key: int):
     new_letters = []
     new_key = key % 26
     for letter in word:
         new_letters.append(get_new_letter(letter, new_key))
     return new_letters
 
+
 def get_new_letter(letter, key):
     new_letter_code = ord(letter) + key
     return chr(new_letter_code) if new_letter_code <= 122 else chr(96 + new_letter_code % 122)
+
 
 print(caesar_cipher(word, key))
