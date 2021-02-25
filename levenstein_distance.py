38f350d747ac1c880206f7f00f11c6cba5f5d018
haxul <s.s.starodubov@yandex.ru>
2021-02-25 20:18:13 +0400

    FF

---
+++ levenstein_distance.py
@@ -0,0 +1,17 @@
+
+
+
+def levenshteinDistance(str1: str, str2: str) -> int:
+    edits = [[x for x in range(len(str1) + 1)] for y in range(len(str2) + 1)]
+    for i in range(1, len(str2) + 1):
+        edits[i][0] = edits[i  - 1][0] + 1
+    for i in range(1, len(str2) + 1):
+        for j in range(1, len(str1) + 1):
+            if str2[i - 1] == str1[j - 1]:
+                edits[i][j] = edits[i -1][j -1]
+            else:
+                edits[i][j] = 1 + min(edits[i - 1][j - 1], edits[i][j -1], edits[i -1][j])
+    return edits[-1][-1]
+
+
+print(levenshteinDistance("abc", "yabd"))
