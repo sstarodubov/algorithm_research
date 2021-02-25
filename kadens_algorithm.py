2cf7e868a3a0a60752f12b796063fb5e71c96e09
haxul <s.s.starodubov@yandex.ru>
2021-02-25 21:28:26 +0400

    FF

--- kadens_algorithm.py
+++ kadens_algorithm.py
@@ -1,13 +1,11 @@
 
 #Space O(1) Time O(n)
 def kadene_algorithm(nums: [int]) -> int:
-    max_subset_sum = 0
-    cur_sum = 0
-    for idx in range(len(nums)):
-        if idx != 0:
-            if nums[idx] + cur_sum > nums[idx]:cur_sum += nums[idx]
-            else: cur_sum = nums[idx]
-        else: cur_sum += nums[idx]
+    max_subset_sum = nums[0]
+    cur_sum = nums[0]
+    for idx in range(1, len(nums)):
+        if nums[idx] + cur_sum > nums[idx]:cur_sum += nums[idx]
+        else: cur_sum = nums[idx]
         if max_subset_sum < cur_sum: max_subset_sum = cur_sum
     return max_subset_sum
 
