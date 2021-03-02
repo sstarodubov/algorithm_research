64375fea8f2731977e15a43a055322f178b5f602
haxul <s.s.starodubov@yandex.ru>
2021-03-02 21:08:43 +0400

    FF

--- youngest_common_ancestor.py
+++ youngest_common_ancestor.py
@@ -26,21 +26,23 @@ graph = {
     "a": None
 }
 
-def define_depth(graph: {}, el: str, level:int = 0):
+
+def define_depth(graph: {}, el: str, level: int = 0):
     if graph[el] is None:
         return level
-    return define_depth(graph, graph[el] , level + 1)
+    return define_depth(graph, graph[el], level + 1)
 
-def up(graph:{}, from_level, to_level, el):
+
+def up(graph: {}, from_level, to_level, el):
     if from_level == to_level:
         return graph[el]
     return up(graph, from_level + 1, to_level, graph[el])
 
 
-#Time O(h) - h - heigh of tree, Space O(h)
+# Time O(h) - h - heigh of tree, Space O(h)
 def youngest_common_ancestor(graph: {str, str}, a: str, b: str):
     level_a = define_depth(graph, a)
-    level_b = define_depth(graph,b)
+    level_b = define_depth(graph, b)
     cur_a_ancestor = graph[a]
     cur_b_ancestor = graph[b]
     if level_b < level_a:
