050b5f58b3dda2df44c25219fd097283228435e5
haxul <s.s.starodubov@yandex.ru>
2021-03-03 21:10:14 +0400

    FF

--- remove_nth_node_from_end.py
+++ remove_nth_node_from_end.py
@@ -5,20 +5,27 @@ class Node:
     def __init__(self, _val):
         self.val = _val
 
-# Time O(n) Space O(1)
+
 class LinkedList:
     head: Node = None
+    size:int = 0
 
     def add(self, val: int):
         new_node: Node = Node(val)
         new_node.prev = self.head
         self.head = new_node
+        self.size += 1
 
     def display(self):
         cur_node = self.head
         while cur_node is not None:
             print(cur_node.val)
             cur_node: Node = cur_node.prev
+
+    def remove_from_end(self, index):
+        new_inx = (self.size - index) - 1
+        if new_inx >=0:
+            self.remove_at(new_inx)
 
     def remove_at(self, index):
         behind_node = None
