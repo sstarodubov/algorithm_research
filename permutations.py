085f886edf4182e811a2c7300d02e8bc020f1c50
haxul <s.s.starodubov@yandex.ru>
2021-03-04 20:49:53 +0400

    FF

---
+++ permutations.py
@@ -0,0 +1,27 @@
+
+def swap(arr, i, y):
+    arr[i], arr[y] = arr[y], arr[i]
+
+def cicle_first_element(arr):
+    i = 0
+    idx_to_swap = i + 1
+    while idx_to_swap != len(arr):
+        print(arr)
+        swap(arr, i, idx_to_swap)
+        idx_to_swap += 1
+        i += 1
+    print(arr)
+    print("---------------")
+    tmp = arr.pop()
+    arr.insert(0, tmp)
+
+def permutation(arr: [int]) -> [[int]]:
+   ptr = 0
+   cicle_first_element(arr)
+   while ptr < len(arr) - 1:
+       ptr += 1
+       swap(arr, 0, ptr)
+       cicle_first_element(arr)
+
+
+permutation([1,2,3,4])
