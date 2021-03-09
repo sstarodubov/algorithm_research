5231b23060ab537575840ecef6a49b143fa9a352
haxul <s.s.starodubov@yandex.ru>
2021-03-09 20:47:00 +0400

    oX

--- python/min_max_stack_construction.py
+++ python/min_max_stack_construction.py
@@ -1,19 +1,18 @@
-
-
 class Stack:
-    values:[int] = []
-    max_size:int
+    values: [int] = []
+    max_size: int
     cur_size = -1
+
     def __init__(self, _max_size):
         self.max_size = _max_size
         self.min_max_stack = []
 
-    def push(self, val:int):
+    def push(self, val: int):
         if self.max_size <= self.cur_size:
             raise ValueError("StackOverflow")
         self.cur_size += 1
         new_min_max = {"max": val, "min": val}
-        if (self.cur_size > 0):
+        if self.cur_size > 0:
             old_min_max = self.min_max_stack[self.cur_size]
             new_min_max["max"] = max(old_min_max["max"], new_min_max["max"])
             new_min_max["min"] = min(old_min_max["min"], new_min_max["max"])
@@ -32,4 +31,3 @@ class Stack:
         if self.cur_size == -1:
             raise ValueError("Stack is empty")
         return self.values[self.cur_size]
-
