7c1a66c21f1ad103c3a9fec5f240a11db0902972
haxul <s.s.starodubov@yandex.ru>
2021-03-11 21:21:11 +0400

    oX

---
+++ python/group_anagrams.py
@@ -0,0 +1,34 @@
+
+# Time O(n * lg(n)) and Space O(1)
+def is_word_anagram(original, word:str):
+    return sorted(original) == sorted(word)
+
+#  Time O(n^2 lg(n)) and Space O(n)
+def find_group(array:[str], word:str):
+    group = [word]
+    removed_indexes = []
+    for el_idx in range(len(array)):
+        if is_word_anagram(word, array[el_idx]):
+            group.append(array[el_idx])
+            removed_indexes.append(el_idx)
+    for idx in reversed(removed_indexes):
+        del array[idx]
+    return group
+
+# Time O(n^3) and Space O(n)
+def group_anagrams(array):
+    result = []
+    while array:
+        word = array.pop()
+        group = find_group(array, word)
+        result.append(group)
+    return result
+
+
+print(group_anagrams(["yo", "act", "blop", "tac", "cat", "oy", "olbp"]))
+
+
+
+
+
+
