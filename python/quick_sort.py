2d747e8f32fd5da9a567dd24003e41945af48f07
haxul <s.s.starodubov@yandex.ru>
2021-03-17 20:29:37 +0400

    oX

---
+++ python/quick_sort.py
@@ -0,0 +1,35 @@
+def make_pivot(arr: [int], left_bound, right_bound):
+    pivot = arr[left_bound]
+    left_ptr = left_bound + 1
+    right_ptr = right_bound
+
+    while left_ptr <= right_ptr:
+        if arr[left_ptr] > pivot > arr[right_ptr]:
+            arr[left_ptr], arr[right_ptr] = arr[right_ptr], arr[left_ptr]
+            right_ptr -= 1
+            left_ptr += 1
+            continue
+
+        if arr[left_ptr] <= pivot:
+            left_ptr += 1
+
+        if arr[right_ptr] > pivot:
+            right_ptr -= 1
+
+    arr[left_bound], arr[right_ptr] = arr[right_ptr], arr[left_bound]
+    return right_ptr
+
+# time: worst = O(n ^ 2), best = O(n * lg n), avg = O(n * lg n)
+# space: O(lg n)
+def quick_sort(arr:[int], left_bound, right_bound):
+    if left_bound > right_bound:
+        return arr
+
+    pivot_idx = make_pivot(arr, left_bound, right_bound)
+    quick_sort(arr, left_bound, pivot_idx - 1)
+    quick_sort(arr, pivot_idx + 1, right_bound)
+    return arr
+
+arr = [10,9,8,6,5,4,3,2,1]
+
+print(quick_sort(arr, 0, len(arr) - 1))
