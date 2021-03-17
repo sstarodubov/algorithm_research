ac05f5dd2a840e0e5dad5b4a43c24c113fdba851
haxul <s.s.starodubov@yandex.ru>
2021-03-17 22:15:21 +0400

    oX

--- python/subarray_sort.py
+++ python/subarray_sort.py
@@ -18,7 +18,7 @@ def sub_array_sort(arr:[int]):
     while arr[right_bound] >= arr[right_ptr]:
         right_ptr += 1
 
-    print(left_ptr + 1, right_ptr - 1)
+    return left_ptr + 1, right_ptr - 1
 
 
 sub_array_sort([1,2,4,7,10,11,7,12,6,7,16,18,19])
