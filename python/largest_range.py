845bc55abc60132709d6b8473c0fd900d365f752
haxul <s.s.starodubov@yandex.ru>
2021-03-18 20:32:04 +0400

    oX

--- python/largest_range.py
+++ python/largest_range.py
@@ -28,4 +28,30 @@ def largest_range_v1(arr: [int]):
     print(cur_result)
 
 
-largest_range_v1([1, 11, 3, 0, 15, 5, 2, 4, 10, 7, 12, 6])
+def largest_range_v2(arr:[int]):
+    storage = {}
+    minimum = float("inf")
+    maximum = float("-inf")
+    for a in arr:
+        minimum = min(minimum, a)
+        maximum = max(maximum, a)
+        storage[a] = a
+    return largest_range_v2_helper(storage, minimum, minimum, (0,0), maximum)
+
+
+def largest_range_v2_helper(storage:{}, start:int, end:int, cur_result:(), limit):
+    if end == limit:
+        return cur_result
+    next_element = storage.get(end + 1)
+    if next_element is not None:
+        return largest_range_v2_helper(storage, start, next_element, cur_result, limit)
+    cur_element = storage.get(end)
+    if cur_element is None:
+        return largest_range_v2_helper(storage, start, end + 1, cur_result, limit)
+    old_diff = cur_result[1] - cur_result[0]
+    new_diff = end - start
+    if new_diff > old_diff:
+        cur_result = (start, end)
+    return largest_range_v2_helper(storage, cur_element, cur_element + 1, cur_result, limit)
+
+print(largest_range_v2([1,11,3,0,15,5,2,4,10,7,12,6]))
