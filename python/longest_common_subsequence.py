26001154eccb0b750e42dba851230b9504ada785
haxul <s.s.starodubov@yandex.ru>
2021-03-25 22:00:22 +0400

    oX

--- python/longest_common_subsequence.py
+++ python/longest_common_subsequence.py
@@ -10,7 +10,31 @@ def lcs_v2(str1, str2):
                 lcs[i][j] = max(lcs[i -1][j], lcs[i][j -1], key=len)
     return lcs[-1][-1]
 
+# Time O(nm) and Space O(nm)
+def lcs_v3(str1, str2):
+    lsc = [[[None, 0, None, None] for x in range(len(str1) + 1)] for y in range(len(str2) + 1)]
+    for i in range(1, len(str2) + 1):
+        for j in range(1, len(str1) + 1):
+            if str2[i - 1] == str1[j - 1]:
+                lsc[i][j] = [str2[i - 1], lsc[i-1][j-1][1] + 1, i - 1, j - 1]
+            else:
+                if lsc[i- 1][j][1] > lsc[i][j -1][1]:
+                    lsc[i][j] = [None, lsc[i - 1][j][1], i - 1, j]
+                else:
+                    lsc[i][j] = [None, lsc[i][j - 1][1], i, j - 1]
+    return build_seq(lsc)
 
+def build_seq(lcs):
+    seq = []
+    i = len(lcs) - 1
+    j = len(lcs[0]) - 1
+    while i != 0 and j != 0:
+        cur_entry = lcs[i][j]
+        if cur_entry[0] is not None:
+            seq.append(cur_entry[0])
+        i = cur_entry[2]
+        j = cur_entry[3]
+    return list(reversed(seq))
 # Time O(nm * min(n,m)) and Space O(nm * min(n,m))
 def longest_common_subsequence(str1, str2):
     matrix = [["" for b in str2] for a in str1]
@@ -29,4 +53,4 @@ def longest_common_subsequence(str1, str2):
     return matrix[len(str1) - 1][len(str2) - 1]
 
 
-print(lcs_v2("xkykzpw", "zxvvyzw"))
+print(lcs_v3("xkykzpw", "zxvvyzw"))
