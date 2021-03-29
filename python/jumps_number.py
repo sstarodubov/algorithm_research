41e0b62fac41d431aa11a116ebb41ffdbfc6176c
haxul <s.s.starodubov@yandex.ru>
2021-03-29 22:14:18 +0400

    oX

--- python/jumps_number.py
+++ python/jumps_number.py
@@ -30,5 +30,19 @@ def min_number_jumps_dp(array: [int]):
                 jumps[i] = min(jumps[j] + 1, jumps[i])
     return jumps[-1]
 
+#Space O(1) and Time O(n)
+def min_number_of_jumps(array:[int]):
+    if len(array) == 1:
+        return 0
+    jumps = 0
+    max_reach = array[0]
+    steps = array[0]
+    for i in range(1, len(array) - 1):
+        max_reach = max(max_reach, i + array[i])
+        steps -= 1
+        if steps == 0:
+            jumps += 1
+            steps = max_reach - i
+    return jumps + 1
 
-print(min_number_jumps_dp([3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3]))
+print(min_number_of_jumps([3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3]))
