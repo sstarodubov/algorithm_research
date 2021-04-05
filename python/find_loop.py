13bdafc866ba23d7519e254382c293468d8d22b2
haxul <s.s.starodubov@yandex.ru>
2021-04-05 15:36:40 +0400

    oX

---
+++ python/find_loop.py
@@ -0,0 +1,31 @@
+class Node:
+
+    def __init__(self, val:int):
+        self.val = val
+        self.next = None
+
+    def __str__(self):
+        return f"Node({self.val})"
+
+
+n1 = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+n4 = Node(4)
+n5 = Node(5)
+n6 = Node(6)
+n7 = Node(7)
+n8 = Node(8)
+n9 = Node(9)
+
+n1.next = n2
+n2.next = n3
+n3.next = n4
+n4.next = n5
+n5.next = n6
+n6.next = n7
+n7.next = n8
+n8.next = n9
+n9.next = n4
+
+print(n1)
