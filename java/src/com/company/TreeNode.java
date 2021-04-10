e5dc6155f7a9ca3c7d42e2614e73f42f188d6315
haxul <s.s.starodubov@yandex.ru>
2021-04-10 22:29:44 +0400

    s

---
+++ java/src/com/company/TreeNode.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    @Override
+    public String toString() {
+        return "Node(" + this.val + ")";
+    }
+
+    public static TreeNode buildBinaryTree(Integer[] nums) {
+        if (nums.length == 0) {
+            throw new UnsupportedOperationException("cannot create tree without input nums");
+        }
+        int p = 0;
+        var root = new TreeNode(nums[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (true) {
+            TreeNode cur = queue.poll();
+            if (cur == null) {
+                throw new UnsupportedOperationException("cannot create node because of null");
+            }
+            if (cur.left == null) {
+                p++;
+                if (p >= nums.length) return root;
+                if (nums[p] != null) {
+                    var node = new TreeNode(nums[p]);
+                    cur.left = node;
+                    queue.add(node);
+                }
+            }
+
+            if (cur.right == null) {
+                p++;
+                if (p >= nums.length) return root;
+                if (nums[p] != null) {
+                    var node = new TreeNode(nums[p]);
+                    cur.right = node;
+                    queue.add(node);
+                }
+            }
+        }
+    }
+}
