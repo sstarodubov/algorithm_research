7cda27b77bf1d3266060e1cff40543cec38e8c05
haxul <s.s.starodubov@yandex.ru>
2021-04-12 21:35:52 +0400

    s

--- java/src/com/company/SecondMinimumNodeInBinaryTree.java
+++ java/src/com/company/SecondMinimumNodeInBinaryTree.java
@@ -1,42 +1,33 @@
 package com.company;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-
 public class SecondMinimumNodeInBinaryTree {
 
     public static void main(String[] args) {
-        TreeNode node = TreeNode.buildBinaryTree(new Integer[]{2147483646,2147483647, 2147483646});
+        TreeNode node = TreeNode.buildBinaryTree(new Integer[]{2,2,5,null,null,5,7});
         var s = new SecondMinimumNodeInBinaryTree();
         int r = s.findSecondMinimumValue(node);
-        assert 2147483647 == r : "2";
-
-
-        TreeNode n = TreeNode.buildBinaryTree(new Integer[]{5,5,6});
-        int r2 = s.findSecondMinimumValue(n);
-        assert 6 == r2 : "6";
+        assert  5 == r;
         System.out.println("tests passed");
     }
 
-    public int findSecondMinimumValue(TreeNode root) {
-        List<Integer> xs = new LinkedList<>();
-        findSecondMinimumValue(root, xs);
-        Collections.sort(xs);
-        int c = 0;
-        for (int i = 0; i < xs.size(); i++) {
-            if (i != 0) {
-                if (!xs.get(i).equals(xs.get(i - 1))) c++;
-            } else c++;
-            if (c == 2) return xs.get(i);
+    int tmp;
+    long ans = Long.MAX_VALUE;
+
+    public void dfs(TreeNode root) {
+        if (root != null) {
+            if (tmp < root.val && root.val < ans) {
+                ans = root.val;
+            } else if (tmp == root.val) {
+                dfs(root.left);
+                dfs(root.right);
+            }
         }
-        return -1;
     }
 
-    public void findSecondMinimumValue(TreeNode node, List<Integer> xs) {
-        if (node == null) return;
-        findSecondMinimumValue(node.left, xs);
-        xs.add(node.val);
-        findSecondMinimumValue(node.right, xs);
+    public int findSecondMinimumValue(TreeNode root) {
+        tmp = root.val;
+        dfs(root);
+        return ans < Long.MAX_VALUE ? (int) ans : -1;
     }
 }
+
