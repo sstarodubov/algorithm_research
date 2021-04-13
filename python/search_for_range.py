ddf3150160d73dac49ecd309d4f511df6c38880a
haxul <s.s.starodubov@yandex.ru>
2021-04-13 18:08:59 +0400

    h

--- python/search_for_range.py
+++ python/search_for_range.py
@@ -1,6 +1,8 @@
 input = [0, 1, 21, 33, 45, 45, 45, 45, 45, 45, 61, 71, 73]
 
-def search_for_range(arr: [int], l: int, r: int, k: int, t:[int, int], found : bool= False):
+
+# time O(lg n) space O(lg n)
+def search_for_range(arr: [int], l: int, r: int, k: int, t: [int, int], found: bool = False):
     if r <= l: return t
     m = (r + l) // 2
     if arr[m] == k:
@@ -17,14 +19,13 @@ r = search_for_range(input, 0, len(input) - 1, 45, [0, len(input) - 1])
 assert 4 == r[0], "first ele eq 4"
 assert 9 == r[1], "second ele eq 9"
 
-assert 0 == search_for_range([0,0,0,1,2,3], 0, 5, 0, [0,5])[0]
-assert 2 == search_for_range([0,0,0,1,2,3], 0, 5, 0, [0,5])[1]
+assert 0 == search_for_range([0, 0, 0, 1, 2, 3], 0, 5, 0, [0, 5])[0], "left edge [0]"
+assert 2 == search_for_range([0, 0, 0, 1, 2, 3], 0, 5, 0, [0, 5])[1], "left edge [1]"
 
-assert 3 == search_for_range([0,1,2,3,3,3], 0, 6, 3, [0,6])[0]
-assert 5 == search_for_range([0,1,2,3,3,3], 0, 6, 3, [0,6])[1]
+assert 3 == search_for_range([0, 1, 2, 3, 3, 3], 0, 6, 3, [0, 6])[0], "right edge [0]"
+assert 5 == search_for_range([0, 1, 2, 3, 3, 3], 0, 6, 3, [0, 6])[1], "right edge [1]"
 
-
-assert -1 == search_for_range([0,1,2,3,3,3], 0, 6, 10, [0,6])[0]
-assert -1 == search_for_range([0,1,2,3,3,3], 0, 6, 10, [0,6])[1]
+assert -1 == search_for_range([0, 1, 2, 3, 3, 3], 0, 6, 10, [0, 6])[0], "not exist [0]"
+assert -1 == search_for_range([0, 1, 2, 3, 3, 3], 0, 6, 10, [0, 6])[1], "not exist [1]"
 
 print("tests passed")
