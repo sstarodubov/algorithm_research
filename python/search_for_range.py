89405b9d4910a8ff7fe296323d37a6b9a0e05fdd
haxul <s.s.starodubov@yandex.ru>
2021-04-13 18:25:21 +0400

    h

--- python/search_for_range.py
+++ python/search_for_range.py
@@ -1,5 +1,3 @@
-input = [0, 1, 21, 33, 45, 45, 45, 45, 45, 45, 61, 71, 73]
-
 
 # time O(lg n) space O(lg n)
 def search_for_range(arr: [int], l: int, r: int, k: int, t: [int, int]):
@@ -8,12 +6,14 @@ def search_for_range(arr: [int], l: int, r: int, k: int, t: [int, int]):
     if arr[m] == k:
         if t[1] >= m: t[1] = m
         if t[0] < m: t[0] = m
-    search_for_range(arr, l, m - 1, k, t)
-    search_for_range(arr, m + 1, r, k, t)
+        search_for_range(arr, l, m - 1, k, t)
+        search_for_range(arr, m + 1, r, k, t)
+    if k < arr[m]: search_for_range(arr, l, m - 1, k, t)
+    else: search_for_range(arr, m + 1, r, k, t)
     return [t[1], t[0]]
 
 
-r = search_for_range(input, 0, len(input) - 1, 45, [0, len(input) - 1])
+r = search_for_range([0, 1, 21, 33, 45, 45, 45, 45, 45, 45, 61, 71, 73], 0, 12, 45, [0, 12])
 
 assert 4 == r[0], "first ele eq 4"
 assert 9 == r[1], "second ele eq 9"
