dafcfd22a98ca104e5d2dc6d293583ac0acb5840
haxul <s.s.starodubov@yandex.ru>
2021-04-14 16:23:50 +0400

    h

---
+++ python/quick_select.py
@@ -0,0 +1,8 @@
+def quick_select(arr: [int], k: int):
+    pass
+
+
+assert 5 == quick_select([8, 5, 2, 9, 7, 6, 3], 3), "3"
+assert 2 == quick_select([8, 5, 2, 9, 7, 6, 3], 1), "2"
+assert 6 == quick_select([8, 5, 2, 9, 7, 6, 3], 4), "6"
+assert 9 == quick_select([8, 5, 2, 9, 7, 6, 3], 7), "9"
