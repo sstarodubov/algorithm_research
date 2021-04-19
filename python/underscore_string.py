8f5b0b9a75838a0967ac098e30fc3943e254395a
starodubov <s.s.starodubov@yandex.ru>
2021-04-19 19:54:18 +0400

    a

--- python/underscore_string.py
+++ python/underscore_string.py
@@ -1,31 +1,23 @@
-
-# time O(n) space O(1)
-def indexof(s: str, w: str, start: int = 0) -> int:
-    try:
-        return s.index(w, start)
-    except:
-        return -1
-
 # space O(n) and time O(n^2)
 def underscore_string(s: str, word: str) -> str:
-    out = ""
-    str_prt = s.index(word)
+    buffer: [str] = []
+    str_prt = s.find(word)
     end_ptr = str_prt + len(word) if str_prt != -1 else -1
     cur_ptr = 0
     while cur_ptr < len(s):
         if cur_ptr == end_ptr:
             if end_ptr != str_prt:
-                out += "_"
+                buffer.append("_")
             end_ptr = str_prt + len(word) if str_prt != 1 else -1
 
         if cur_ptr == str_prt:
-            if indexof(s, word, str_prt - len(word)) != str_prt - len(word):
-                out += "_"
-            str_prt = indexof(s, word, cur_ptr + 1)
+            if s.find(word, str_prt - len(word)) != str_prt - len(word):
+                buffer.append("_")
+            str_prt = s.find(word, cur_ptr + 1)
 
-        out += s[cur_ptr]
+        buffer.append(s[cur_ptr])
         cur_ptr += 1
-    return out
+    return "".join(buffer)
 
 
 s = "testthis is a testtest to see if testtesttest it works"
