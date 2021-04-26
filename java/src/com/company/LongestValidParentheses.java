cd02580cecbdd937e592bfc9fed9a9e78c9f69ee
starodubov <s.s.starodubov@yandex.ru>
2021-04-26 20:31:24 +0400

    of

--- java/src/com/company/LongestValidParentheses.java
+++ java/src/com/company/LongestValidParentheses.java
@@ -2,13 +2,14 @@ package com.company;
 
 
 import java.util.ArrayDeque;
+import java.util.BitSet;
 
 public class LongestValidParentheses {
 
     public static void main(String[] args) {
         var l = new LongestValidParentheses();
 
-        assert 6 == l.longestValidParentheses("()(()))") : 0;
+        assert 6 == l.longestValidParentheses("()(())") : 0;
         assert 2 == l.longestValidParentheses("(()") : 1;
         assert 4 == l.longestValidParentheses(")()())") : 2;
         assert 0 == l.longestValidParentheses("") : 3;
@@ -18,43 +19,33 @@ public class LongestValidParentheses {
     }
 
     // Space O(n) and Space O(n)
+
     public int longestValidParentheses(String s) {
-        int[] mask = new int[s.length()];
-        var stack = new ArrayDeque<Pair>();
+        BitSet mask = new BitSet(s.length());
+        var stack = new ArrayDeque<Integer>();
         for (int i = 0; i < s.length(); i++) {
             if (stack.isEmpty()) {
-                var pair = new Pair(s.charAt(i), i);
-                stack.push(pair);
+                stack.push(i);
             } else {
-                var last = stack.peek();
-                if (last.p == '(' && s.charAt(i) == ')') {
+                var lastIdx = stack.peek();
+                if (s.charAt(lastIdx) == '(' && s.charAt(i) == ')') {
                     stack.pop();
-                } else stack.push(new Pair(s.charAt(i), i));
+                } else stack.push(i);
             }
         }
-        for (var pair : stack) {
-            mask[pair.idx] = 1;
+        for (var i :stack) {
+            mask.set(i);
         }
 
         int ans = 0;
         int cur = 0;
-        for (int bit : mask) {
-            if (bit == 1) {
+        for (int i = 0; i < s.length(); i++) {
+            if (mask.get(i)) {
                 ans = Math.max(ans, cur);
                 cur = 0;
             } else cur++;
         }
 
         return Math.max(ans, cur);
-    }
-
-    private class Pair {
-        int idx;
-        char p;
-
-        Pair(char p, int idx) {
-            this.idx = idx;
-            this.p = p;
-        }
     }
 }
