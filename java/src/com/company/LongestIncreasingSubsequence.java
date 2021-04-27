a775767b25978ed401998417745bcbe05d6782fd
starodubov <s.s.starodubov@yandex.ru>
2021-04-27 16:47:07 +0400

    of

---
+++ java/src/com/company/LongestIncreasingSubsequence.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class LongestIncreasingSubsequence {
+    public static void main(String[] args) {
+        var el = new LongestIncreasingSubsequence();
+        assert 4 == el.lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18}) : 1;
+        assert 4 == el.lengthOfLIS(new int[]{0, 1, 0, 3, 2, 3}) : 2;
+        assert 1 == el.lengthOfLIS(new int[]{7, 7, 7, 7, 7, 7, 7}) : 3;
+        System.out.println("tests passed");
+    }
+
+    // Time O(n ^ 2 ) and Space O(n)
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) return 0;
+        var lens = new int[nums.length];
+        lens[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            int cur = i;
+            int len = 0;
+            while (cur >= 0) {
+                if (nums[i] > nums[cur]) {
+                    len = Math.max(len, lens[cur]);
+                }
+                cur--;
+            }
+            lens[i] = len + 1;
+        }
+
+        int m = 0;
+        for (var l : lens) {
+            if (l > m) m = l;
+        }
+        return m;
+    }
+}
