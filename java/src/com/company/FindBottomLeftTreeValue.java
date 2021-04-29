ce89143e7cd49b63014c3711df36a20cf788e29e
starodubov <s.s.starodubov@yandex.ru>
2021-04-29 17:27:35 +0400

    of

--- java/src/com/company/FindBottomLeftTreeValue.java
+++ java/src/com/company/FindBottomLeftTreeValue.java
@@ -1,36 +1,28 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class FindBottomLeftTreeValue {
     public static void main(String[] args) {
         var m = new FindBottomLeftTreeValue();
-        assert 7 == m.findBottomLeftValue(TreeNode.buildBinaryTree(new Integer[] {1,2,3,4,null,5,6,null,null,7})) : 2;
-        assert 1 == m.findBottomLeftValue(TreeNode.buildBinaryTree(new Integer[]{2,1,3})) : 1;
+        assert 7 == m.findBottomLeftValue(TreeNode.buildBinaryTree(new Integer[]{1, 2, 3, 4, null, 5, 6, null, null, 7})) : 2;
+        assert 1 == m.findBottomLeftValue(TreeNode.buildBinaryTree(new Integer[]{2, 1, 3})) : 1;
         System.out.println("tests passed");
     }
 
-    // Space O(n) and Time O(n)
+    // Space O(log d) d - depth of tree and Time O(n) n - amount of tree nodes
     public int findBottomLeftValue(TreeNode root) {
-        var list = new ArrayList<int[]>();
-        helper(root, 0, list);
-        int d = 0;
-        int ans = 0;
-        for (var pair : list) {
-            if (pair[1] > d) {
-                d = pair[1];
-                ans = pair[0];
-            }
-        }
-        return ans;
+        int[] pair = new int[2];
+        helper(root, 1, pair);
+        return pair[0];
     }
 
-    public void helper(TreeNode node, int depth, List<int[]> out) {
+    public void helper(TreeNode node, int depth, int[] pair) {
         if (node != null) {
-            helper(node.left, depth + 1, out);
-            out.add(new int[] {node.val, depth});
-            helper(node.right, depth + 1, out);
+            helper(node.left, depth + 1, pair);
+            if (pair[1] < depth) {
+                pair[1] = depth;
+                pair[0] = node.val;
+            }
+            helper(node.right, depth + 1, pair);
         }
     }
 }
