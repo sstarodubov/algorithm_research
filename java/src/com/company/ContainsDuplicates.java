7a74c2e6181fc1567ddc37603d742f48133d67df
starodubov <s.s.starodubov@yandex.ru>
2021-05-08 23:23:09 +0400

    of

--- java/src/com/company/ContainsDuplicates.java
+++ java/src/com/company/ContainsDuplicates.java
@@ -1,16 +1,33 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class ContainsDuplicates {
     public static void main(String[] args) {
 
         var main = new ContainsDuplicates();
 
+        assert main.containsNearbyDuplicate(new int[]{1, 2, 3, 1}, 3) : 11;
+        assert !main.containsNearbyDuplicate(new int[]{1, 2, 3, 1, 2, 3}, 2);
+
         assert main.containsDuplicate(new int[]{1, 2, 3, 1}) : 0;
         assert !main.containsDuplicate(new int[]{1, 2, 3, 4}) : 1;
 
         System.out.println("tests passed");
+    }
+
+    // Time O(n) and Space O(n)
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        var t = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (t.containsKey(nums[i])) {
+                int diff = Math.abs(i - t.get(nums[i]));
+                if (diff <= k) return true;
+            }
+            t.put(nums[i], i);
+        }
+        return false;
     }
 
 
@@ -25,6 +42,4 @@ public class ContainsDuplicates {
         }
         return false;
     }
-
-
 }
