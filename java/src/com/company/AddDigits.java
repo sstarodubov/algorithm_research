23cce5ce8efce3515df6833e1a939b6f6b63aeb7
starodubov <s.s.starodubov@yandex.ru>
2021-05-08 22:28:09 +0400

    of

---
+++ java/src/com/company/AddDigits.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class AddDigits {
+    public static void main(String[] args) {
+
+        var m = new AddDigits();
+
+        assert 2 == m.addDigits(38) : 0;
+        assert 0 == m.addDigits(0) : 1;
+
+        System.out.println("tests passed");
+    }
+
+    public int addDigits(int num) {
+        if (num == 0) return 0;
+        if (num % 9 == 0) return 9;
+        return num % 9;
+    }
+}
