0f4d0a582fdd963cb55ca7d5d10d7eb3bd2053df
starodubov <s.s.starodubov@yandex.ru>
2021-05-10 18:08:58 +0400

    of

--- java/src/com/company/ContainsDuplicates.java
+++ java/src/com/company/ContainsDuplicates.java
@@ -2,31 +2,47 @@ package com.company;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.TreeSet;
 
 public class ContainsDuplicates {
     public static void main(String[] args) {
 
         var main = new ContainsDuplicates();
 
+        TreeSet<Integer> set = new TreeSet<>();
+
+        set.add(10);
+        set.add(20);
+        set.add(30);
+
+        System.out.println(set.ceiling(11));
 
         assert main.containsNearbyAlmostDuplicate(new int[]{8, 7, 15, 1, 6, 1, 9, 15}, 1, 3);
         assert main.containsNearbyAlmostDuplicate(new int[]{1, 2, 3, 1}, 3, 0) : 99;
         assert !main.containsNearbyAlmostDuplicate(new int[]{1, 5, 9, 1, 5, 9}, 2, 3) : 91;
 
-       assert main.containsNearbyDuplicate(new int[]{1, 2, 3, 1}, 3) : 11;
-       assert !main.containsNearbyDuplicate(new int[]{1, 2, 3, 1, 2, 3}, 2);
+        assert main.containsNearbyDuplicate(new int[]{1, 2, 3, 1}, 3) : 11;
+        assert !main.containsNearbyDuplicate(new int[]{1, 2, 3, 1, 2, 3}, 2);
 
-       assert main.containsDuplicate(new int[]{1, 2, 3, 1}) : 0;
-       assert !main.containsDuplicate(new int[]{1, 2, 3, 4}) : 1;
+        assert main.containsDuplicate(new int[]{1, 2, 3, 1}) : 0;
+        assert !main.containsDuplicate(new int[]{1, 2, 3, 4}) : 1;
 
         System.out.println("tests passed");
     }
 
+    // TIme O(n * log n  ) and Space O(k)
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        TreeSet<Long> tree = new TreeSet<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (Math.abs(i - j) <= k && Math.abs(nums[i] - nums[j]) <= t) return true;
-            }
+            Long floor = tree.floor((long) nums[i]);
+            if (floor != null && Math.abs(nums[i] - floor) <= t) return true;
+
+            Long ceiling = tree.ceiling((long) nums[i]);
+
+            if (ceiling != null && Math.abs(nums[i] - ceiling) <= t) return true;
+
+            tree.add((long) nums[i]);
+            if (tree.size() > k) tree.remove((long) nums[i - k]);
         }
         return false;
     }
