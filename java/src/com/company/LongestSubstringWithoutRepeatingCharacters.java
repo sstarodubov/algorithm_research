bc71ff1a9738ad7de06c5c9c2361252cedb3db7d
starodubov <s.s.starodubov@yandex.ru>
2021-05-11 20:49:56 +0400

    of

--- java/src/com/company/LongestSubstringWithoutRepeatingCharacters.java
+++ java/src/com/company/LongestSubstringWithoutRepeatingCharacters.java
@@ -6,16 +6,17 @@ public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
         var m = new LongestSubstringWithoutRepeatingCharacters();
 
+        assert 5 == m.lengthOfLongestSubstring("tmmzuxt") : 2000;
         assert 3 == m.lengthOfLongestSubstring("dvdf") : -1000;
-        assert 1 == m.lengthOfLongestSubstring(" ") : 1000;
         assert 3 == m.lengthOfLongestSubstring("pwwkew") : -1;
+        assert 1 == m.lengthOfLongestSubstring(" ") : 1000;
         assert 1 == m.lengthOfLongestSubstring("bbbbb") : 1;
         assert 3 == m.lengthOfLongestSubstring("abcabcbb") : 0;
         assert 0 == m.lengthOfLongestSubstring("") : 9;
         System.out.println("tests passed");
     }
 
-    // Time O(n^2) and Space O(n)
+    // Time O(n) and Space O(n)
     public int lengthOfLongestSubstring(String s) {
         if (s.isEmpty()) return 0;
         var table = new HashMap<Character, Integer>();
@@ -23,17 +24,18 @@ public class LongestSubstringWithoutRepeatingCharacters {
         int max = 0;
         for (int i = 0; i < s.length(); i++) {
             char letter = s.charAt(i);
-            if (!table.containsKey(letter)) {
+            if (!table.containsKey(letter) || (table.containsKey(letter) && table.get(letter)< start)) {
                 table.put(letter, i);
+                if (i == s.length() - 1) {
+                    max = Math.max(max, (i - start) + 1);
+                }
             } else {
                 max = Math.max(max, i - start);
                 int d = table.get(letter);
                 start = d + 1;
-                i = d + 1;
-                table.clear();
-                table.put(s.charAt(i), i);
+                table.put(letter, i);
             }
         }
-        return Math.max(max, table.size());
+        return max;
     }
 }
