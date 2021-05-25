ab847b49743f681540a787756be90caa73477300
starodubov <s.s.starodubov@yandex.ru>
2021-05-25 16:30:00 +0400

    of

--- java/src/com/company/NumberofLongestIncreasingSubsequence.java
+++ java/src/com/company/NumberofLongestIncreasingSubsequence.java
@@ -15,62 +15,35 @@ public class NumberofLongestIncreasingSubsequence {
     }
 
     public int findNumberOfLIS(int[] nums) {
-
-
-        // Base Case
         if (nums.length == 0)
             return 0;
-
         int n = nums.length;
-
-        // Initialize dp_l array with
-        // 1s
-        int[] dp_l = new int[n];
-        Arrays.fill(dp_l, 1);
-
-        // Initialize dp_c array with
-        // 1s
-        int[] dp_c = new int[n];
-        Arrays.fill(dp_c, 1);
-
+        int[] lens = new int[n];
+        Arrays.fill(lens, 1);
+        int[] counts = new int[n];
+        Arrays.fill(counts, 1);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
-
-                // If current element is
-                // smaller
                 if (nums[i] <= nums[j])
                     continue;
-
-                if (dp_l[j] + 1 > dp_l[i]) {
-                    dp_l[i] = dp_l[j] + 1;
-                    dp_c[i] = dp_c[j];
-                } else if (dp_l[j] + 1 == dp_l[i])
-                    dp_c[i] += dp_c[j];
+                if (lens[j] + 1 > lens[i]) {
+                    lens[i] = lens[j] + 1;
+                    counts[i] = counts[j];
+                } else if (lens[j] + 1 == lens[i])
+                    counts[i] += counts[j];
             }
         }
 
-        // Store the maximum element
-        // from dp_l
-        int max_length = 0;
-
-        for (int i : dp_l)
-            max_length = Math.max(i, max_length);
-
-        // Stores the count of LIS
+        int maxLength = 0;
+        for (int i : lens) {
+            maxLength = Math.max(i, maxLength);
+        }
         int count = 0;
-
-        // Traverse dp_l and dp_c
-        // simultaneously
         for (int i = 0; i < n; i++) {
-
-            // Update the count
-            if (dp_l[i] == max_length)
-                count += dp_c[i];
+            if (lens[i] == maxLength)
+                count += counts[i];
         }
 
-        // Return the count of LIS
         return count;
     }
-
-
 }
