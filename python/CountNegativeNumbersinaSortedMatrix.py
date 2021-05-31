401b5aac9debf92582ec3626c0aae082811a1003
starodubov <s.s.starodubov@yandex.ru>
2021-05-31 20:03:18 +0400

    a

--- python/CountNegativeNumbersinaSortedMatrix.py
+++ python/CountNegativeNumbersinaSortedMatrix.py
@@ -3,16 +3,29 @@ from typing import List
 
 class Solution:
 
-    # Time O(n^2) Space O(1)
+    # Time O(n * log n) Space O(1)
     def countNegatives(self, grid: List[List[int]]) -> int:
         ans = 0
         for i in range(len(grid) - 1, -1, -1):
-            for j in range(len(grid[i]) - 1, -1, -1):
-                if grid[i][j] >= 0:
-                    break
-                else:
-                    ans += 1
+            idx = self.indexOfFirstNegative(grid[i])
+            if idx == -1:
+                continue
+            else:
+                ans += len(grid[i]) - idx
         return ans
+
+    def indexOfFirstNegative(self, arr: List[int]) -> int:
+        l = 0
+        r = len(arr) - 1
+        m = (l + r) // 2
+        while l < r:
+            if arr[m] >= 0:
+                l = m + 1
+            else:
+                r = m
+            m = (l + r) // 2
+        if arr[r] >= 0: return -1
+        return r
 
 
 s = Solution()
