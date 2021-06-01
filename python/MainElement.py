73d86bb70615d6b49b725994ff7a0d41b24cbe5f
starodubov <s.s.starodubov@yandex.ru>
2021-06-01 20:42:54 +0400

    a

--- python/MainElement.py
+++ python/MainElement.py
@@ -1,25 +1,27 @@
-import math
 from typing import List
 
 
 class Solution:
-    # Time O(n) Space O(n)
-    def majorityElement(self, nums: List[int]) -> int:
-        d = {}
-        maj = math.ceil(len(nums) / 2)
 
+    # Time O(n) and Space O(n)
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        d = {}
+        t = {}
         for n in nums:
             if n not in d:
                 d[n] = 1
             else:
                 d[n] += 1
-                if d[n] == maj:
-                    return n
-        return nums[0]
+            if d[n] > (len(nums) / 3):
+                t[n] = True
+
+        return [num for num in t.keys()]
+
 
 s = Solution()
 
-assert 2 == s.majorityElement([2, 2, 1, 1, 1, 2, 2]), "2"
-assert 3 == s.majorityElement([3, 2, 3]), "3"
+assert [3] == s.majorityElement([3, 2, 3, 3, 3, 3]), "2"
+assert [1, 2] == s.majorityElement([1, 2]), "1"
+assert [3] == s.majorityElement([3]), "3"
 
 print("tests passed")
