911696fbdea52d558abec9300424c578f05cc76c
starodubov <s.s.starodubov@yandex.ru>
2021-06-19 20:29:30 +0400

    df

--- kotlin/src/main/kotlin/GoodCountTriplets.kt
+++
@@ -1,8 +0,0 @@
-
-fun main() {
-
-}
-
-fun countGoodTriplets(arr: IntArray, a: Int, b: Int, c: Int): Int {
-    return 1
-}
---
+++ kotlin/src/main/kotlin/GreatesCommonDelimeter.kt
@@ -0,0 +1,10 @@
+fun greatestCommonDelimiter(a: Int, b: Int): Int {
+    var a: Int = a
+    var b: Int = b
+    while (b != 0) {
+        val reminder: Int = a % b
+        a = b
+        b = reminder
+    }
+    return a
+}
