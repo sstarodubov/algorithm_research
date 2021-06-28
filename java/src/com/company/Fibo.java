ed71a7e7ab24093949d343e1706e072ef2583190
haxul <s.s.starodubov@yandex.ru>
2021-06-28 21:20:32 +0400

    as

---
+++ java/src/com/company/Fibo.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Fibo {
+    public static void main(String[] args) {
+
+
+        System.out.println("tests passed");
+    }
+
+    public int fib(int n) {
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+}
