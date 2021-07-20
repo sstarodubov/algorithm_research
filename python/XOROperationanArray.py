1aad6e6187348c978168bd81ce149ce2bc920cc2
haxul <s.s.starodubov@yandex.ru>
2021-07-20 21:56:34 +0400

    №1486

--- python/XOROperationanArray.py
+++ python/XOROperationanArray.py
@@ -1,3 +1,12 @@
+"""
+Given an integer n and an integer start.
+
+Define an array nums where nums[i] = start + 2*i (0-indexed) and n == nums.length.
+
+Return the bitwise XOR of all elements of nums.
+"""
+
+
 class Solution:
     def xorOperation(self, n: int, start: int) -> int:
         arr = []
