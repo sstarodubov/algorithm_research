1c9f1dc54d9cc16437c33454d998a2ce1262645b
haxul <s.s.starodubov@yandex.ru>
2021-08-04 20:15:14 +0400

    №70

--- python/ClaimbingStairs.py
+++ python/ClaimbingStairs.py
@@ -6,20 +6,13 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
 
 class Solution:
 
-    # CPU O(n) and RAM O(n)
+    # CPU O(n) and RAM O(1)
     def climbStairs(self, n: int) -> int:
-        arr = [0 for _ in range(n)]
-        if len(arr) == 0:
-            return 0
-        if len(arr) == 1:
-            return 1
-        if len(arr) == 2:
-            return 2
-        arr[0] = 1
-        arr[1] = 2
-        for i in range(2, len(arr)):
-            arr[i] = arr[i - 1] + arr[i - 2]
-        return arr[len(arr) - 1]
+        if n == 1: return 1
+        x, y = 1, 2
+        for i in range(3, n + 1):
+            x, y = y, x + y
+        return y
 
 
 s = Solution()
