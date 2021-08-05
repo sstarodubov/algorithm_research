f9cac34e98ba4d93946f4b1fe2bf94e6969c3208
haxul <s.s.starodubov@yandex.ru>
2021-08-05 22:14:20 +0400

    №876

--- python/MiddleoftheLinkedList.py
+++ python/MiddleoftheLinkedList.py
@@ -4,24 +4,12 @@ Given the head of a singly linked list, return the middle node of the linked lis
 If there are two middle nodes, return the second middle node.
 
 """
-
-
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.next = next
-        self.val = val
-
-    def __eq__(self, other):
-        return self.val == other.val
-
-    def __str__(self):
-        return f"Node({self.val})"
+from utils.ListNode import ListNode
 
 
 class Solution:
 
-    #cpu O(n) and ram O(1)
+    # cpu O(n) and ram O(1)
     def middleNode(self, head: ListNode) -> ListNode:
         slow = fast = head
         while True:
@@ -35,8 +23,6 @@ class Solution:
 
 s = Solution()
 
-node = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, ListNode(6))))))
-
-assert ListNode(3) == s.middleNode(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))), 2
-assert ListNode(4) == s.middleNode(node), 1
+assert ListNode(3) == s.middleNode(ListNode.build([1, 2, 3, 4, 5])), 2
+assert ListNode(4) == s.middleNode(ListNode.build([1, 2, 3, 4, 5, 6])), 1
 print("tests passed")
---
+++ python/utils/ListNode.py
@@ -0,0 +1,32 @@
+from collections import deque
+from typing import List, Optional, Deque
+
+"""
+class to build single linked list from int array
+"""
+
+
+class ListNode:
+
+    def __init__(self, val=0, next: "ListNode" = None):
+        self.next = next
+        self.val = val
+
+    def __eq__(self, other):
+        return self.val == other.val
+
+    def __str__(self):
+        return f"Node({self.val})"
+
+    @staticmethod
+    def build(xs: List[int]) -> Optional["ListNode"]:
+        xs = deque(xs)
+
+        def _helper(xs: Deque[int]) -> Optional["ListNode"]:
+            if not xs:
+                return None
+            head, *tail = xs
+            return ListNode(head, ListNode.build(tail))
+
+        return _helper(xs)
+
