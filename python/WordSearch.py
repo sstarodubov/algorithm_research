2fa392685ea186943e3e58cce7b1fa2337170e45
haxul <s.s.starodubov@yandex.ru>
2021-09-01 21:35:11 +0400

    Revert "№79"
    This reverts commit fba3bff2f03f692736f63a307fd003a1c93b01ac.

--- python/WordSearch.py
+++ python/WordSearch.py
@@ -1,36 +1,89 @@
 from typing import List
 
+"""
+public class Solution {
+    static boolean[][] visited;
+    public boolean exist(char[][] board, String word) {
+        visited = new boolean[board.length][board[0].length];
+        
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                if((word.charAt(0) == board[i][j]) && search(board, word, i, j, 0)){
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    private boolean search(char[][]board, String word, int i, int j, int index){
+        if(index == word.length()){
+            return true;
+        }
+        
+        if(i >= board.length || i < 0 || j >= board[i].length || j < 0 || board[i][j] != word.charAt(index) || visited[i][j]){
+            return false;
+        }
+        
+        visited[i][j] = true;
+        if(search(board, word, i-1, j, index+1) || 
+           search(board, word, i+1, j, index+1) ||
+           search(board, word, i, j-1, index+1) || 
+           search(board, word, i, j+1, index+1)){
+            return true;
+        }
+        
+        visited[i][j] = false;
+        return false;
+    }
+}
+"""
+
+
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        used = [[False for _ in col] for col in board]
+        def startswith(xs, ys):
+            if len(ys) > len(xs):
+                return False
+            for i in range(len(ys)):
+                if ys[i] != xs[i]:
+                    return False
+            return True
 
-        def found(y, x, idx: int) -> bool:
+        used = [[False for _ in col] for col in board]
+        word_list = list(word)
+
+        def found(cur_word: [str], cell: (int, int)) -> bool:
             # found solution
-            if idx == len(word):
+            if word_list == cur_word:
                 return True
             # validation
+            y, x = cell
             if y < 0 or x < 0:
                 return False
             if y >= len(board) or x >= len(board[0]):
                 return False
 
-            if word[idx] != board[y][x]:
-                return False
-            if used[y][x]:
-                return False
-            # apply
-            used[y][x] = True
-            if found(y - 1, x, idx + 1) or \
-                    found(y + 1, x, idx + 1) or \
-                    found(y, x + 1, idx + 1) or \
-                    found(y, x - 1, idx + 1):
-                return True
-            used[y][x] = False
+            if ((startswith(word_list, cur_word)) and not used[y][x]) or cur_word == []:
+                # apply
+                cur_word.append(board[y][x])
+                used[y][x] = True
+                if found(cur_word, (y - 1, x)):
+                    return True
+                if found(cur_word, (y + 1, x)):
+                    return True
+                if found(cur_word, (y, x + 1)):
+                    return True
+                if found(cur_word, (y, x - 1)):
+                    return True
+                cur_word.pop()
+                used[y][x] = False
             return False
 
         for y in range(len(board)):
             for x in range(len(board[0])):
-                if word[0] == board[y][x] and found(y, x, 0):
+                if word[0] == board[y][x] and found([], (y, x)):
                     return True
         return False
 
