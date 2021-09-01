1afd05b8088681f91ef7827de354288fa98f6711
haxul <s.s.starodubov@yandex.ru>
2021-09-01 21:35:19 +0400

    Revert "№79"
    This reverts commit f43eb0dd584890e7bc1a61bc94214cbf6846eeea.

--- python/WordSearch.py
+++ python/WordSearch.py
@@ -1,45 +1,5 @@
 from typing import List
 
-"""
-public class Solution {
-    static boolean[][] visited;
-    public boolean exist(char[][] board, String word) {
-        visited = new boolean[board.length][board[0].length];
-        
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){
-                if((word.charAt(0) == board[i][j]) && search(board, word, i, j, 0)){
-                    return true;
-                }
-            }
-        }
-        
-        return false;
-    }
-    
-    private boolean search(char[][]board, String word, int i, int j, int index){
-        if(index == word.length()){
-            return true;
-        }
-        
-        if(i >= board.length || i < 0 || j >= board[i].length || j < 0 || board[i][j] != word.charAt(index) || visited[i][j]){
-            return false;
-        }
-        
-        visited[i][j] = true;
-        if(search(board, word, i-1, j, index+1) || 
-           search(board, word, i+1, j, index+1) ||
-           search(board, word, i, j-1, index+1) || 
-           search(board, word, i, j+1, index+1)){
-            return true;
-        }
-        
-        visited[i][j] = false;
-        return false;
-    }
-}
-"""
-
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
@@ -79,13 +39,16 @@ class Solution:
                     return True
                 cur_word.pop()
                 used[y][x] = False
+                if not cur_word:
+                    y += 1
+                    if y >= len(board):
+                        y = 0
+                        x += 1
+                    return found(cur_word, (y, x))
             return False
 
-        for y in range(len(board)):
-            for x in range(len(board[0])):
-                if word[0] == board[y][x] and found([], (y, x)):
-                    return True
-        return False
+        ans = found([], (0, 0))
+        return ans
 
 
 s = Solution()
