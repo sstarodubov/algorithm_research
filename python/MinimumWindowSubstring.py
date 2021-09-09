3d7d6bd6cab772fc342b43e92a8ef7240f5ae13e
haxul <s.s.starodubov@yandex.ru>
2021-09-09 21:33:31 +0400

    №76

--- python/MinimumWindowSubstring.py
+++ python/MinimumWindowSubstring.py
@@ -4,41 +4,61 @@ from collections import Counter
 class Solution:
 
     def minWindow(self, s: str, t: str) -> str:
-        window = [""] * len(t)
-        c = Counter(t)
-        origin = Counter(t)
-        begin = 0
-        while len(window) <= len(s):
-            if begin + len(window) > len(s):
-                begin = 0
-                window.append("")
-            if len(window) > len(s):
-                return ""
-            for i in range(len(window)):
-                window[i] = s[begin + i]
-            begin += 1
-            for w in window:
-                if w in c:
-                    c[w] -= 1
-            flag = False
+        def exist(st, l, r, subs):
+            c = Counter(subs)
+            for i in range(l, r + 1):
+                if r >= len(st):
+                    return False
+                if st[i] in c:
+                    c[st[i]] -= 1
             for k in c:
                 if c[k] > 0:
-                    flag = True
-                c[k] = origin[k]
-            if flag:
-                flag = False
+                    return False
+            return True
+
+        if len(s) == len(t):
+            if exist(s, 0, len(s) - 1, t):
+                return s
             else:
-                return "".join(window)
-        return ""
+                return ""
+        left = 0
+        right = len(t) - 1
+        result = ""
+        while left < len(s) - len(t) and right < len(s):
+            flag = False
+            while right < len(s):
+                if exist(s, left, right, t):
+                    flag = True
+                    break
+                else:
+                    right += 1
+            if not flag:
+                return result
+            while left < len(s):
+                if not exist(s, left + 1, right, t):
+                    break
+                else:
+                    left += 1
+            tmp = s[left:right + 1]
+            if result:
+                if len(tmp) < len(result):
+                    result = tmp
+            else:
+                result = tmp
+            left += 1
+            right += 1
+        return result
 
 
 s = Solution()
 
-assert s.minWindow("abc", "ac") == "abc", 1
-assert s.minWindow("a", "b") == "", 0
-assert s.minWindow("aaaaaaaaaaaabbbbbcdd", "abcdd") == "abbbbbcdd", 100
-assert s.minWindow(s="ADOBECODEBANC", t="ABC") == "BANC", 1
+assert s.minWindow("abc", "a") == "a", 123123
+assert s.minWindow("ab", "A") == "", -100
 assert s.minWindow(s="a", t="a") == "a", 0
 assert s.minWindow(s="a", t="aa") == "", -1
+assert s.minWindow("aaaaaaaaaaaabbbbbcdd", "abcdd") == "abbbbbcdd", 100
+assert s.minWindow("a", "b") == "", 0
+assert s.minWindow("abc", "ac") == "abc", 1
+assert s.minWindow(s="ADOBECODEBANC", t="ABC") == "BANC", 1
 
 print("tests passed")
