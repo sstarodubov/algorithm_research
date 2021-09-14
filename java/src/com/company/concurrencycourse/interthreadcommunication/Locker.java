a4f508664b65516a9aa2bb6b5e297e44dc0d45d2
haxul <s.s.starodubov@yandex.ru>
2021-09-14 20:03:35 +0400

    livelock

--- java/src/com/company/concurrencycourse/interthreadcommunication/Locker.java
+++ java/src/com/company/concurrencycourse/interthreadcommunication/Locker.java
@@ -3,6 +3,61 @@ package com.company.concurrencycourse.interthreadcommunication;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+
+class Livelock {
+    private static final Lock lock1 = new ReentrantLock();
+    private static final Lock lock2 = new ReentrantLock();
+
+    public static void main(String[] args) {
+        var l = new Livelock();
+        new Thread(l::worker1).start();
+        new Thread(l::worker2).start();
+    }
+
+    public void worker1() {
+        while (true) {
+            System.out.println("working1");
+            if (lock1.tryLock()) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (lock2.tryLock()) {
+                    break;
+                }
+            } else {
+                lock1.unlock();
+            }
+        }
+        lock1.unlock();
+        lock2.unlock();
+        System.out.println("finish 1");
+    }
+
+    public void worker2() {
+        while (true) {
+            System.out.println("working2");
+            if (lock2.tryLock()) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (lock1.tryLock()) {
+                    break;
+                }
+            } else {
+                lock2.unlock();
+            }
+
+        }
+        lock2.unlock();
+        lock1.unlock();
+        System.out.println("finish 2");
+    }
+}
+
 public class Locker {
 
     private static int counter = 0;
