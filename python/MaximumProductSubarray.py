37e305b17b1a34c93a9c45edb39f23b941a5919b
haxul <s.s.starodubov@yandex.ru>
2021-09-28 22:06:44 +0400

    №152

--- python/MaximumProductSubarray.py
+++ python/MaximumProductSubarray.py
@@ -13,19 +13,19 @@ A subarray is a contiguous subsequence of the array.
 
 class Solution:
 
-    # cpu O(n ^ 3)
+    # cpu O(n ^ 2)
     def maxProduct(self, nums: List[int]) -> int:
         result = nums[0]
         for i in range(len(nums)):
+            cur_prod = 1
             for y in range(i, len(nums)):
-                tmp = 1
-                for k in range(i, y + 1):
-                    tmp = tmp * nums[k]
-                result = max(result, tmp)
+                cur_prod *= nums[y]
+                result = max(result, cur_prod)
         return result
 
 
 s = Solution()
 assert 6 == s.maxProduct([2, 3, -2, 4]), 1
 assert 0 == s.maxProduct([-2, 0, -1]), 0
+
 print("tests passed")
