007184517f08a725e91c54150356177ba24f5ea0
haxul <s.s.starodubov@yandex.ru>
2021-10-22 21:11:23 +0400

    #783

--- python/leetcode/783. Minimum Distance Between BST Nodes.py
+++ python/leetcode/783. Minimum Distance Between BST Nodes.py
@@ -5,31 +5,34 @@ from leetcode.tree_node import TreeNode
 
 class Solution:
 
-    # cpu O(n) and ram O(n)
-    def minDiffInBST(self, node: Optional[TreeNode]) -> int:
-        diff = 999999999999
-        nodes = []
+    # cpu O(n) and ram O(log d)
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        self.prev = None
+        self.cur = None
+        self.diff = 10 ** 9
 
-        def helper(n: TreeNode):
-            if not n:
+        def helper(node: TreeNode):
+            if not node:
                 return
-            helper(n.left)
-            nodes.append(n.val)
-            helper(n.right)
+            helper(node.left)
+            if self.prev is None:
+                self.prev = node.val
+            elif self.cur is None:
+                self.cur = node.val
+                self.diff = self.cur - self.prev
+            else:
+                self.prev = self.cur
+                self.cur = node.val
+                self.diff = min(self.diff, self.cur - self.prev)
+            helper(node.right)
 
-        helper(node)
-        if len(nodes) == 1:
-            return nodes[0]
-        f, s = 0, 1
-        while s < len(nodes):
-            cur_diff = nodes[s] - nodes[f]
-            diff = min(cur_diff, diff)
-            f += 1
-            s += 1
-
-        return diff
+        helper(root)
+        return self.diff
 
 
+assert Solution().minDiffInBST(TreeNode.build_tree([0, None, 2236, 1277, 2776, 519])) == 519
+assert Solution().minDiffInBST(TreeNode.build_tree([90, 69, None, 49, 89, None, 52])) == 1
+assert Solution().minDiffInBST(TreeNode.build_tree([27, None, 34, None, 58, 50, None, 44])) == 6
 assert Solution().minDiffInBST(TreeNode.build_tree([4, 2, 6, 1, 3])) == 1
 assert Solution().minDiffInBST(TreeNode.build_tree([1, 0, 48, None, None, 12, 49])) == 1
 print("tests passed")
