808bc07bc8e4436e2b31bc5479da35ebdf2aa48b
haxul <s.s.starodubov@yandex.ru>
2021-10-22 19:43:42 +0400

    #94

---
+++ python/leetcode/94. Binary Tree Inorder Traversal.py
@@ -0,0 +1,27 @@
+from typing import Optional, List
+
+from leetcode.tree_node import TreeNode
+
+
+class Solution:
+
+    # ram (log n) and cpu O(n)
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        if not root:
+            return ans
+
+        def helper(node: TreeNode, arr):
+            if not node:
+                return
+            helper(node.left, arr)
+            arr.append(node.val)
+            helper(node.right, arr)
+
+        helper(root, ans)
+        return ans
+
+
+assert Solution().inorderTraversal(TreeNode.build_tree([1, None, 2, 3])) == [1, 3, 2]
+
+print("tests passed")
