702da1ab5e7094c546b6e0adb8e0dd5fca173c60
haxul <s.s.starodubov@yandex.ru>
2021-10-25 22:12:56 +0400

    #98

---
+++ python/leetcode/98. Validate Binary Search Tree.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from leetcode.tree_node import TreeNode
+
+
+class Solution:
+
+    # cpu O(nodes) ram O(depth)
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+        def helper(node: TreeNode, min_bound: int, max_bound: int):
+            if not node:
+                return True
+            if node.val >= max_bound:
+                return False
+            if node.val <= min_bound:
+                return False
+            return helper(node.left, min_bound, node.val) and helper(node.right, node.val, max_bound)
+
+        return helper(root, float("-inf"), float("inf"))
+
+
+assert not Solution().isValidBST(TreeNode.build_tree([5, 4, 6, None, None, 3, 7]))
+assert not Solution().isValidBST(TreeNode.build_tree([2, 2, 2]))
+assert Solution().isValidBST(TreeNode.build_tree([10, 5, 11, 2, 9, None, None, None, 3]))
+assert Solution().isValidBST(TreeNode.build_tree([2, 1, 3]))
+assert not Solution().isValidBST(TreeNode.build_tree([5, 1, 4, None, None, 3, 6]))
+print("tests passed")
