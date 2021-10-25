86a47c6e8d20da855b2948a200a6a8ce4d40936a
haxul <s.s.starodubov@yandex.ru>
2021-10-25 21:33:39 +0400

    #501

--- python/leetcode/501. Find Mode in Binary Search Tree.py
+++ python/leetcode/501. Find Mode in Binary Search Tree.py
@@ -11,27 +11,38 @@ class Solution:
         if not root:
             return []
         modes = []
-        mx = {}
-        maximum = 0
-        queue = deque()
-        queue.append(root)
+        self.maximum = 0
+        self.prev = None
+        self.count = 0
 
-        while queue:
-            cur: TreeNode = queue.popleft()
-            cur_amount = mx.get(cur.val, 0)
-            mx[cur.val] = cur_amount + 1
-            maximum = max(maximum, mx[cur.val])
+        def helper(cur_node: TreeNode, modes):
+            if not cur_node:
+                return
 
-            if cur.left:
-                queue.append(cur.left)
-            if cur.right:
-                queue.append(cur.right)
-        for k in mx:
-            if mx[k] == maximum:
-                modes.append(k)
+            helper(cur_node.left, modes)
+
+            if self.prev:
+                if self.prev.val == cur_node.val:
+                    self.count += 1
+                else:
+                    self.count = 0
+
+            if self.count > self.maximum:
+                self.maximum = self.count
+                modes.clear()
+                modes.append(cur_node.val)
+            elif self.count == self.maximum:
+                modes.append(cur_node.val)
+            self.prev = cur_node
+
+            helper(cur_node.right, modes)
+
+        helper(root, modes)
+
         return modes
 
 
+assert Solution().findMode(TreeNode.build_tree([1, None, 2])) == [1, 2]
 assert Solution().findMode(TreeNode.build_tree([0])) == [0]
 assert Solution().findMode(TreeNode.build_tree([1, None, 2, 2])) == [2]
 print("tests passed")
