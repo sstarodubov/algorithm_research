195b237e27a0bca7d1d7b7706bd0209ac27a8ec6
haxul <s.s.starodubov@yandex.ru>
2021-11-04 20:04:12 +0400

    df

--- java/src/com/company/reactor/src/main/java/haxul/sec09/Main.java
+++ java/src/com/company/reactor/src/main/java/haxul/sec09/Main.java
@@ -9,17 +9,26 @@ import java.time.Duration;
 public class Main {
     @SneakyThrows
     public static void main(String[] args) {
-//        Flux.interval(Duration.ofMillis(300))
-//                .map(i -> "event " + i)
-//                .buffer(5)
-//                .subscribe(DefaultSubscriber.subscriber());
+        Flux.interval(Duration.ofMillis(300))
+                .map(i -> "event " + i)
+                .buffer(5)
+                .subscribe(DefaultSubscriber.subscriber());
 
         Flux.interval(Duration.ofMillis(300))
                 .map(i -> "event: " + i)
                 .window(5)
                 .subscribe(DefaultSubscriber.subscriber());
 
+        Flux.range(1, 30)
+                .delayElements(Duration.ofSeconds(1))
+                .groupBy(i -> i & 1)
+                .subscribe(gf -> process(gf, gf.key()));
+
 
         Thread.sleep(100000);
+    }
+
+    private static void process(Flux<Integer> flux, int key) {
+        flux.subscribe(i -> System.out.println("Key: " + key + ", Item: " + i));
     }
 }
