70376c24bfe534ca0b56b8f69f39e2ff0d6ae101
haxul <s.s.starodubov@yandex.ru>
2021-11-07 16:31:32 +0400

    hello

--- asm_tasks/lesson1.asm
+++ asm_tasks/lesson1.asm
@@ -7,11 +7,24 @@ public _start
 ; ax - 16 bit
 ; ah/al - 9 bit
 
+; byte - 8 bit
+; word - 16 bit
+; dword - 32 bit
+; qword - 64 bit
+
+msg db "hello world!!!", 0
+len = $-msg
+
 _start:
+    mov rax, 4 ; write
+    mov rbx, 1 ; 1 - std out
+    mov rcx, msg
+    mov rdx, len
+    int 0x80
     call exit
 exit:
-    mov rax, 1
-    mov rbx, 0
+    mov rax, 1 ; 1 - exit
+    mov rbx, 0 ; 0 - return
     int 0x80
 
 
