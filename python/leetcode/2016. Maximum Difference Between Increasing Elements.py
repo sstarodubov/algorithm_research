22dad6ed0d2a85bb8d2c5d5ca772a88acc56b14f
haxul <s.s.starodubov@yandex.ru>
2021-11-09 21:33:05 +0400

    #2016

--- python/leetcode/2016. Maximum Difference Between Increasing Elements.py
+++ python/leetcode/2016. Maximum Difference Between Increasing Elements.py
@@ -2,15 +2,20 @@ from typing import List
 
 
 class Solution:
-    # O(n ^ 2)
+    # O(n)
     def maximumDifference(self, nums: List[int]) -> int:
+        lptr, rptr = 0, 1
         ans = -1
-        for i in range(len(nums)):
-            for y in range(i + 1, len(nums)):
-                ans = max(ans, nums[y] - nums[i])
-        return -1 if ans <= 0 else ans
+        while rptr < len(nums):
+            if nums[lptr] >= nums[rptr]:
+                lptr = rptr
+            ans = max(ans, nums[rptr] - nums[lptr])
+            rptr += 1
+
+        return ans if ans > 0 else -1
 
 
+assert Solution().maximumDifference([87, 68, 91, 86, 58, 63, 43, 98, 6, 40]) == 55
 assert Solution().maximumDifference([9, 4, 3, 2]) == -1
 assert Solution().maximumDifference([7, 1, 5, 4]) == 4
 assert Solution().maximumDifference([1, 5, 2, 10]) == 9
