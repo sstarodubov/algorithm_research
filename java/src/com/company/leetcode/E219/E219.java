0de346a90d584188eccf02a13fbed612fa62d959
haxul <s.s.starodubov@yandex.ru>
2021-11-12 22:13:34 +0400

    #219

--- java/src/com/company/leetcode/E219/E219.java
+++ java/src/com/company/leetcode/E219/E219.java
@@ -18,15 +18,12 @@ class Solution {
         if (k == 0) return false;
         var set = new HashSet<Integer>();
         for (int i = 0; i < nums.length; i++) {
-            if (set.size() <= k) {
-                if (set.contains(nums[i])) return true;
-                else set.add(nums[i]);
-            } else {
+            if (set.size() > k) {
                 int last = nums[i - k - 1];
                 set.remove(last);
-                if (set.contains(nums[i])) return true;
-                else set.add(nums[i]);
             }
+            if (set.contains(nums[i])) return true;
+            else set.add(nums[i]);
         }
         return false;
     }
