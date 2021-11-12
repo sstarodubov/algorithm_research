c4b7d51b95a2a51dae08c774dcd15091a1e16acd
haxul <s.s.starodubov@yandex.ru>
2021-11-12 21:51:22 +0400

    #239

---
+++ java/src/com/company/leetcode/E219/E219.java
@@ -0,0 +1,14 @@
+package com.company.leetcode.E219;
+
+public class E219 {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        return false;
+    }
+}
--- java/src/com/company/leetcode/H239/H239.java
+++ java/src/com/company/leetcode/H239/H239.java
@@ -1,11 +1,12 @@
-package com.company.leetcode;
+package com.company.leetcode.H239;
 
 import java.util.*;
 
 public class H239 {
     public static void main(String[] args) {
-        assert Arrays.equals(new Solution().maxSlidingWindow(new int[]{1, 3, -1, 3, 5}, 3), new int[]{3, 3, 5});
+        assert Arrays.equals(new Solution().maxSlidingWindow(new int[]{1, -1}, 1), new int[]{1, -1});
         assert Arrays.equals(new Solution().maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3), new int[]{3, 3, 5, 5, 6, 7});
+        assert Arrays.equals(new Solution().maxSlidingWindow(new int[]{1, 3, -1, 3, 5}, 3), new int[]{3, 3, 5});
         System.out.println("tests passed");
     }
 }
@@ -13,8 +14,40 @@ public class H239 {
 
 class Solution {
 
-    // time O(n * log n) and space O(n)
     public int[] maxSlidingWindow(int[] nums, int k) {
+        var result = new ArrayList<Integer>(nums.length * 2);
+
+        var dq = new ArrayDeque<Integer>();
+        int left = 0, right = 0;
+        while (right < nums.length) {
+
+            while (!dq.isEmpty() && nums[dq.getLast()] < nums[right]) {
+                dq.removeLast();
+            }
+
+            dq.add(right);
+
+            if (left > dq.getFirst()) {
+                dq.removeFirst();
+            }
+
+            if (right + 1 >= k) {
+                result.add(nums[dq.getFirst()]);
+                left ++;
+            }
+
+            right ++;
+        }
+
+        var ans = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            ans[i] = result.get(i);
+        }
+        return ans;
+    }
+
+    // time O(n * log n) and space O(n)
+    public int[] maxSlidingWindow1(int[] nums, int k) {
         var result = new ArrayList<Integer>(nums.length * 2);
         var tree = new TreeMap<Integer, Integer>(Collections.reverseOrder());
         for (int i = 0; i < k; i++) {
