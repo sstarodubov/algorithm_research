de816c0633dee3d04e009d43de07f3870dfec308
haxul <s.s.starodubov@yandex.ru>
2021-11-12 19:47:05 +0400

    #239

--- java/src/com/company/leetcode/H239.java
+++ java/src/com/company/leetcode/H239.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class H239 {
     public static void main(String[] args) {
+        assert Arrays.equals(new Solution().maxSlidingWindow(new int[]{1, 3, -1, 3, 5}, 3), new int[]{3, 3, 5});
         assert Arrays.equals(new Solution().maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3), new int[]{3, 3, 5, 5, 6, 7});
         System.out.println("tests passed");
     }
@@ -12,24 +13,27 @@ public class H239 {
 
 class Solution {
 
-    // time O(n ^2) and space O(n)
+    // time O(n * log n) and space O(n)
     public int[] maxSlidingWindow(int[] nums, int k) {
         var result = new ArrayList<Integer>(nums.length * 2);
-        var pq = new PriorityQueue<Integer>(k, Collections.reverseOrder());
+        var tree = new TreeMap<Integer, Integer>(Collections.reverseOrder());
         for (int i = 0; i < k; i++) {
-            pq.add(nums[i]);
+            if (tree.containsKey(nums[i])) tree.computeIfPresent(nums[i], (key, val) -> val + 1);
+            else tree.put(nums[i], 1);
         }
 
         int right = k - 1;
         int left = 0;
         while (right < nums.length) {
-            int max = pq.element();
+            int max = tree.firstKey();
             result.add(max);
-            pq.remove(nums[left]);
+            tree.computeIfPresent(nums[left], (key, v) -> v - 1);
+            if (tree.get(nums[left]) == 0) tree.remove(nums[left]);
             left++;
             right++;
             if (right >= nums.length) break;
-            pq.add(nums[right]);
+            if (tree.containsKey(nums[right])) tree.computeIfPresent(nums[right], (key, val) -> val + 1);
+            else tree.put(nums[right], 1);
         }
 
         var ans = new int[result.size()];
