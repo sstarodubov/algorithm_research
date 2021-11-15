72036448c67a226bed0457f5e51f5e3065011e92
haxul <s.s.starodubov@yandex.ru>
2021-11-15 21:49:52 +0400

    asm

--- asm_tasks/main.asm
+++ asm_tasks/main.asm
@@ -1,15 +1,26 @@
 %include "stud_io.inc"
 global _start
 
+section .data
+stext db "hello world"
 
 section .text
-
 _start:
-    mov eax, 0
-again:
-    PRINT "Hello"
-    PUTCHAR 10
-    inc eax
-    cmp eax, 5
-    jl again
+    mov eax , 11
+    call print_word
     FINISH
+
+print_word:
+    PUSH ecx
+    PUSH eax
+    xor ecx, ecx
+
+cycle:
+    PUTCHAR [stext + ecx]
+    inc ecx
+    cmp ecx , eax
+    jl cycle
+
+    POP eax
+    POP ecx
+    ret
