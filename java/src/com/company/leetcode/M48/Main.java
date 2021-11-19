7e30e0ea3812a9e832176396042aae8e0c726b1c
haxul <s.s.starodubov@yandex.ru>
2021-11-19 19:11:16 +0400

    #48

--- java/src/com/company/leetcode/M48/Main.java
+++ java/src/com/company/leetcode/M48/Main.java
@@ -1,26 +1,31 @@
 package com.company.leetcode.M48;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[][] matrix = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         new Solution().rotate(matrix);
+        System.out.println(Arrays.deepToString(matrix));
     }
 }
 
 
 class Solution {
 
-    // time O(n^2) and space O(n)
+    // time O(n^2) and space O(1)
     public void rotate(int[][] matrix) {
-        var m = new int[matrix.length][matrix.length];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                m[i][j] = matrix[i][j];
-            }
-        }
-        for (int row = 0; row < matrix.length; row++) {
-            for (int col = matrix.length - 1, newCol = 0; col >= 0; col--, newCol++) {
-                matrix[row][newCol] = m[col][row];
+        int n = matrix.length;
+        for (int layer = 0; layer < n / 2; layer++) {
+            int first = layer;
+            int last = n - 1 - layer;
+            for (int i = first; i < last; i++) {
+                int offset = i - first;
+                int top = matrix[first][i];
+                matrix[first][i] = matrix[last - offset][first];
+                matrix[last - offset][first] = matrix[last][last - offset];
+                matrix[last][last - offset] = matrix[i][last];
+                matrix[i][last] = top;
             }
         }
     }
