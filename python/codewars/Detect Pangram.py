353d4202182774eb82003bb3d26399f3de70bebe
haxul <s.s.starodubov@yandex.ru>
2021-11-30 23:51:34 +0400

    #codewars

---
+++ python/codewars/Detect Pangram.py
@@ -0,0 +1,15 @@
+import string
+
+
+def is_pangram(s):
+    st = set()
+    for letter in s:
+        if letter.isalpha():
+            st.add(letter.lower())
+    return len(st) == 26
+
+
+assert is_pangram("ABCD45EFGH,IJK,LMNOPQR56STUVW3XYZ")
+assert is_pangram("The quick brown fox jumps over the lazy dog")
+
+print("tests passed")
