79fb1928f985b4c6c902012e15573c232f66d869
haxul <s.s.starodubov@yandex.ru>
2021-12-01 21:17:49 +0400

    #11 leetcode

--- java/src/com/company/leetcode/M11/Main.java
+++ java/src/com/company/leetcode/M11/Main.java
@@ -2,19 +2,29 @@ package com.company.leetcode.M11;
 
 public class Main {
     public static void main(String[] args) {
+        assert new Solution().maxArea(new int[]{1, 2, 1}) == 2;
+        assert new Solution().maxArea(new int[]{1, 3, 2, 5, 25, 24, 5}) == 24;
+        assert new Solution().maxArea(new int[]{4, 3, 2, 1, 4}) == 16;
+        assert new Solution().maxArea(new int[]{1, 1}) == 1;
         assert new Solution().maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}) == 49;
         System.out.println("tests passed");
     }
 }
 
 class Solution {
-    // time O(n ^2)
+    // time O(n) , space O(1)
     public int maxArea(int[] height) {
-        int max = 0;
-        for (int start = 0; start < height.length; start++) {
-            for (int end = start + 1; end < height.length; end++) {
-                max = Math.max(max, Math.min(height[start], height[end]) * (end - start));
+        int l = 0, r = height.length - 1;
+        int max = Math.min(height[l], height[r]) * (r - l);
+
+        while (l < r) {
+            if (height[l] > height[r]) r--;
+            else if (height[r] > height[l]) l++;
+            else {
+                r--;
+                l++;
             }
+            max = Math.max(max, Math.min(height[l], height[r]) * (r - l));
         }
         return max;
     }
