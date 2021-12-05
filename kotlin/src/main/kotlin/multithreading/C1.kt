fe2e28c1f6556945c0a04e7c64c88cf16c09fb95
haxul <s.s.starodubov@yandex.ru>
2021-12-05 13:04:59 +0400

    ds

--- kotlin/src/main/kotlin/multithreading/C1.kt
+++ kotlin/src/main/kotlin/multithreading/C1.kt
@@ -2,7 +2,11 @@ package multithreading
 
 import kotlinx.coroutines.*
 import java.util.concurrent.Executors
+import java.util.concurrent.ThreadFactory
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.system.measureTimeMillis
+
 
 //@OptIn(DelicateCoroutinesApi::class)
 //fun main() = runBlocking {
@@ -24,15 +28,30 @@ import kotlin.system.measureTimeMillis
 //    }
 //}
 
+class MyThreadFactory(private val name: String) : ThreadFactory {
+    private val integer = AtomicInteger(1)
+
+    override fun newThread(r: Runnable): Thread {
+        return Thread(r, "pool-$name-${integer.getAndIncrement()}")
+    }
+}
+
+val pool = Executors.newFixedThreadPool(8, MyThreadFactory("TESTTICHE")).asCoroutineDispatcher()
+
+
 fun main() = runBlocking {
+
+    println(Thread.currentThread().name)
+
     val time = measureTimeMillis {
-        val job = launch {
+        val job = launch(pool) {
             launch { hello("hello", 1000) }
             launch { hello("world", 900) }
         }
         job.join()
     }
     println(time)
+
 }
 
 
