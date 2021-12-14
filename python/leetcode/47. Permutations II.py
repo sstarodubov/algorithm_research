37128cb51a3b101cbcde1181b7aed3026a2f1270
haxul <s.s.starodubov@yandex.ru>
2021-12-14 17:46:40 +0400

    #47 leetcode

--- python/leetcode/47. Permutations II.py
+++ python/leetcode/47. Permutations II.py
@@ -1,28 +1,27 @@
+from collections import Counter
 from typing import List, Set
 
 
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        ans = set()
-        result = []
-        nums.sort()
-        nums = list(map(str, nums))
-        self.backtrack(nums, 0, ans, result)
-        return result
+        ans = []
+        perm = []
+        counter = Counter(nums)
 
-    def backtrack(self, nums: List[int], idx: int, table: Set[int], result):
-        if idx >= len(nums):
-            key: str = "".join(nums[:])
-            if key not in table:
-                result.append(list(map(int, nums[:])))
-                table.add(key)
-        else:
-            for i in range(idx, len(nums)):
-                if idx != i and nums[i] == nums[idx]:
-                    continue
-                nums[idx], nums[i] = nums[i], nums[idx]
-                self.backtrack(nums, idx + 1, table, result)
-                nums[i], nums[idx] = nums[idx], nums[i]
+        def backtrack():
+            if len(perm) == len(nums):
+                ans.append(perm[:])
+            else:
+                for num in counter:
+                    if counter[num] > 0:
+                        counter[num] -= 1
+                        perm.append(num)
+                        backtrack()
+                        counter[num] += 1
+                        perm.pop()
+
+        backtrack()
+        return ans
 
 
 assert Solution().permuteUnique([2, 2, 1, 1]) == [[1, 1, 2, 2], [1, 2, 1, 2], [1, 2, 2, 1], [2, 1, 1, 2], [2, 1, 2, 1],
