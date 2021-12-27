c11e36380efe90aad19a0d292e6bbbbf84e4ad83
haxul <s.s.starodubov@yandex.ru>
2021-12-27 20:00:17 +0400

    #739 leetcode

--- python/leetcode/739. Daily Temperatures.py
+++ python/leetcode/739. Daily Temperatures.py
@@ -2,18 +2,29 @@ from typing import List
 
 
 class Solution:
-    # time O(n ^ 2)
+
+    # time O(n) , and space O(n)
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         ans = [0] * len(temperatures)
-        for i in range(len(temperatures)):
-            for y in range(i, len(temperatures)):
-                if temperatures[y] > temperatures[i]:
-                    ans[i] = y - i
-                    break
+        stack = []
+
+        for cur_idx in range(len(temperatures)):
+            if not stack:
+                stack.append(cur_idx)
+                continue
+
+            while stack and temperatures[cur_idx] > temperatures[stack[len(stack) - 1]]:
+                last_idx = stack.pop()
+                diff = cur_idx - last_idx
+                ans[last_idx] = diff
+
+            stack.append(cur_idx)
+
         return ans
 
 
 assert Solution().dailyTemperatures([30, 60, 90]) == [1, 1, 0]
 assert Solution().dailyTemperatures([30, 40, 50, 60]) == [1, 1, 1, 0]
 assert Solution().dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]) == [1, 1, 4, 2, 1, 1, 0, 0]
+
 print("tests passed")
