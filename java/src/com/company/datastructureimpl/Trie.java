3d837c7a7738b79e5cc87d7bc92d2cab5e8c7dd4
haxul <s.s.starodubov@yandex.ru>
2022-01-02 18:04:42 +0400

    #trie

--- java/src/com/company/datastructureimpl/Trie.java
+++ java/src/com/company/datastructureimpl/Trie.java
@@ -14,6 +14,43 @@ public class Trie {
 
     private final TNode root = new TNode();
 
+    public void delete(String word) {
+        if (exists(word)) delete(root, word, 0);
+    }
+
+    // time O(L), space O(log H)
+    private boolean delete(TNode parent, String word, int idx) {
+        char letter = word.charAt(idx);
+        var curNode = parent.children.get(letter);
+
+        if (curNode.children.size() > 1) {
+            delete(curNode, word, idx + 1);
+            return false;
+        }
+
+        if (idx == word.length() - 1) {
+            if (curNode.children.size() >= 1) {
+                curNode.endOfString = false;
+                return false;
+            }
+            parent.children.remove(letter);
+            return true;
+        }
+        if (curNode.endOfString) {
+            delete(curNode, word, idx + 1);
+            return false;
+        }
+
+        boolean canThisNodeBeDeleted = delete(curNode, word, idx + 1);
+
+        if (canThisNodeBeDeleted) {
+            parent.children.remove(letter);
+            return true;
+        }
+
+        return false;
+    }
+
     // time O(L), space O(1)
     public boolean exists(final String word) {
         var cur = root;
@@ -47,6 +84,8 @@ public class Trie {
         trie.insert("s");
 
         assert trie.exists("app");
+        trie.delete("app");
+        assert !trie.exists("app");
         assert trie.exists("api");
         assert trie.exists("apis");
         assert trie.exists("s");
@@ -54,7 +93,6 @@ public class Trie {
         assert !trie.exists("apiss");
         assert !trie.exists("a");
         assert !trie.exists("");
-
         System.out.println("tests passed");
     }
 }
