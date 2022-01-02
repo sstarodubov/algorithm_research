cd8fd1dd7dc81da552cbfafe7c84562393866f3c
haxul <s.s.starodubov@yandex.ru>
2022-01-02 18:07:23 +0400

    s

---
+++ asm_tasks/lesson1.asm
@@ -0,0 +1,79 @@
+format ELF64 ; executable
+; entry _start
+
+public _start
+
+; rax - 64 bit
+; eax - 32 bit
+; ax - 16 bit
+; ah/al - 9 bit
+
+; byte - 8 bit
+; word - 16 bit
+; dword - 32 bit
+; qword - 64 bit
+section '.data' writeable
+    msg db "Yello world!!!", 0
+
+section '.text' executable
+
+_start:
+    mov rax, msg
+    
+    call print_string
+
+    call exit
+
+section '.print_char' executable
+    mov rax , 4
+    mov rbx, 1
+    mov rcx, 'A'
+    mov rdx, 1
+    int 0x80
+
+; | input
+; rax = string 
+print_string:
+    push rax
+    push rbx
+    push rcx
+    push rdx
+
+    mov rcx, rax
+    
+    call length_string
+    
+    mov rdx, rax
+    mov rax, 4 ; write
+    mov rbx, 1 ; std out
+    
+    int 0x80
+    
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
+
+    ret
+
+; | input
+; rax = string
+; | output
+; rax = lenthg
+length_string:
+    push rdx
+    xor rdx, rdx
+    .next_iter:
+        cmp [rax + rdx], byte 0
+        je .close
+        inc rdx
+        jmp .next_iter
+    .close:
+        mov rax, rdx
+        pop rdx
+        ret
+
+exit:
+    mov rax, 1 ; 1 - exit
+    mov rbx, 0 ; 0 - return
+    int 0x80
