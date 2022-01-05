fc6ccec85430086c97f69ca9f5a74b83e7f4c0bc
haxul <s.s.starodubov@yandex.ru>
2022-01-05 19:04:59 +0400

    #sort

--- java/src/com/company/datastructureimpl/Sorting.java
+++ java/src/com/company/datastructureimpl/Sorting.java
@@ -1,27 +1,38 @@
 package com.company.datastructureimpl;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Sorting {
     public static void main(String[] args) {
         var arr = new int[]{5, 4, 3, 2, 1};
-        bubbleSort(arr);
+        heapSort(arr);
         System.out.println(Arrays.toString(arr));
+    }
+
+    // O (n log n), space O(n)
+    public static void heapSort(int[] arr) {
+        var heap = new PriorityQueue<Integer>(arr.length);
+        for (int j : arr) {
+            heap.add(j);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            assert heap.peek() != null;
+            arr[i] = heap.poll();
+        }
     }
 
     // time O(n^2), space O(1)
     public static void bubbleSort(int[] arr) {
         int end = arr.length;
         for (int i = 0; i < end; ) {
-            int j = i + 1;
-            while (j < end) {
-                if (arr[i] > arr[j]) {
+            while (i + 1 < end) {
+                if (arr[i] > arr[i + 1]) {
                     int tmp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = tmp;
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = tmp;
                 }
                 i++;
-                j++;
             }
             i = 0;
             end--;
