636ad1742c83fc438f1d37d8bd6b61f20dfeeb33
haxul <s.s.starodubov@yandex.ru>
2022-01-05 20:05:54 +0400

    #sort

--- java/src/com/company/datastructureimpl/Sorting.java
+++ java/src/com/company/datastructureimpl/Sorting.java
@@ -6,12 +6,27 @@ import java.util.PriorityQueue;
 public class Sorting {
     public static void main(String[] args) {
         var arr = new int[]{5, 4, 3, 2, 1};
-        selectSort(arr);
+        insertSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     // time O(n^2), space O(1)
-    public static void  selectSort(int[] arr) {
+    public static void insertSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int ptr = i - 1;
+            int cur = i;
+            while (ptr >= 0 && arr[ptr] > arr[cur]) {
+                int tmp = arr[ptr];
+                arr[ptr] = arr[cur];
+                arr[cur] = tmp;
+                ptr--;
+                cur--;
+            }
+        }
+    }
+
+    // time O(n^2), space O(1)
+    public static void selectSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
