323caa2ae3d79078350833a18fbf6610ec96127d
haxul <s.s.starodubov@yandex.ru>
2022-01-18 20:28:51 +0400

    #108 leetcode

---
+++ python/leetcode/108. Convert Sorted Array to Binary Search Tree.py
@@ -0,0 +1,23 @@
+from typing import Optional, List
+
+from leetcode.tree_node import TreeNode
+
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+
+        if not nums:
+            return None
+        if len(nums) == 1:
+            return TreeNode(nums[0])
+
+        middle = len(nums) // 2
+
+        new_node = TreeNode(nums[middle])
+        new_node.left = self.sortedArrayToBST(nums[:middle])
+        new_node.right = self.sortedArrayToBST(nums[middle + 1:])
+
+        return new_node
+
+
+node = Solution().sortedArrayToBST([-10, -3, 0, 5, 9])
