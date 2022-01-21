0695204bf464ff8671d62395cbda50143a8bd987
haxul <s.s.starodubov@yandex.ru>
2022-01-21 16:51:19 +0400

    #14 leetcode

--- python/leetcode/14. Longest Common Prefix.py
+++ python/leetcode/14. Longest Common Prefix.py
@@ -1,23 +1,42 @@
+from collections import defaultdict
 from typing import List
+
+
+class Trie:
+    def __init__(self):
+        self.root = {"end": False}
+
+    def insert(self, word):
+        cur = self.root
+        for letter in word:
+            if letter not in cur:
+                cur[letter] = {"end": False}
+            cur = cur[letter]
+        cur["end"] = True
 
 
 class Solution:
 
-    #time O(n ^2), space O(1)
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        ptr = 0
-        while True:
-            for i in range(len(strs)):
-                if ptr >= len(strs[i]):
-                    return strs[i][:ptr]
+        trie = Trie()
+        for word in strs:
+            trie.insert(word)
 
-                if i == 0:
-                    cur = strs[i][ptr]
-                else:
-                    if cur != strs[i][ptr]:
-                        return strs[i][:ptr]
-            ptr += 1
+        dist = 0
+        cur = trie.root
+        while len(cur) == 2:
+            cur_key = ""
+            for key in cur.keys():
+                if key == "end":
+                    continue
+                cur_key = key
+            if cur["end"]:
+                return strs[0][:dist]
+            cur = cur[cur_key]
+            dist += 1
+        return strs[0][:dist]
 
 
+assert "a" == Solution().longestCommonPrefix(["a", "ab"])
 assert "" == Solution().longestCommonPrefix(["dog", "racecar", "car"])
 assert "fl" == Solution().longestCommonPrefix(["flower", "flow", "flight"])
