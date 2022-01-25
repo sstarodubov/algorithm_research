122651a89957e2945c80edaa7b0fe3eb74bcd77e
haxul <s.s.starodubov@yandex.ru>
2022-01-25 20:25:01 +0400

    #110 leetcode

---
+++ python/leetcode/110. Balanced Binary Tree.py
@@ -0,0 +1,29 @@
+from typing import Optional
+
+from leetcode.tree_node import TreeNode
+
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        self.ans = True
+
+        def dfs(node):
+            if not node:
+                return 0
+            ld = dfs(node.left)
+            rd = dfs(node.right)
+
+            if abs(ld - rd) > 1:
+                self.ans = False
+
+            return max(ld, rd) + 1
+
+        dfs(root)
+
+        return self.ans
+
+
+assert Solution().isBalanced(TreeNode.build_tree([1, 2, 3, 4, 5, 6, None, 8]))
+assert not Solution().isBalanced(TreeNode.build_tree([1, None, 2, None, 3]))
+assert Solution().isBalanced(TreeNode.build_tree([3, 9, 20, None, None, 15, 7]))
+assert not Solution().isBalanced(TreeNode.build_tree([1, 2, 2, 3, 3, None, None, 4, 4]))
