1c7829a6e353bb46d584d518f33bd70940c5850f
haxul <s.s.starodubov@yandex.ru>
2022-01-27 21:52:02 +0400

    #225 leetcode

---
+++ python/leetcode/225. Implement Stack using Queues.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+
+class MyStack:
+
+    def __init__(self):
+        self.q = deque([])
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+
+    def pop(self) -> int:
+        return self.q.pop()
+
+    def top(self) -> int:
+        return self.q[-1]
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
