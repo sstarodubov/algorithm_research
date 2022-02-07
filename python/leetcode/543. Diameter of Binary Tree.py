da269120d95a1e263ae739c3921c7a19bd9cb12d
haxul <s.s.starodubov@yandex.ru>
2022-02-07 22:37:49 +0400

    #543 leetcode

---
+++ python/leetcode/543. Diameter of Binary Tree.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from leetcode.tree_node import TreeNode
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.ans = float("-inf")
+
+        def dfs(node: TreeNode) -> int:
+            if not node:
+                return 0
+
+            lr = dfs(node.left)
+            rr = dfs(node.right)
+
+            self.ans = max(self.ans, lr + rr)
+            mx = max(lr, rr)
+            return mx + 1
+
+        dfs(root)
+        return self.ans
