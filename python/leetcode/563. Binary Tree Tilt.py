91e3f8bc1179404cac3f2005331c766543c67281
haxul <s.s.starodubov@yandex.ru>
2022-02-08 21:25:04 +0400

    #563 leetcode

---
+++ python/leetcode/563. Binary Tree Tilt.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from leetcode.tree_node import TreeNode
+
+
+class Solution:
+    def findTilt(self, root: Optional[TreeNode]) -> int:
+
+        self.s = 0
+
+        def dfs(node: TreeNode) -> int:
+            if not node:
+                return 0
+
+            lv = dfs(node.left)
+            rv = dfs(node.right)
+            t = node.val
+            diff = abs(lv - rv)
+            node.val = diff
+            self.s += node.val
+            return lv + rv + t
+
+        dfs(root)
+
+        return self.s
+
+
+assert 15 == Solution().findTilt(TreeNode.build_tree([4, 2, 9, 3, 5, None, 7]))
