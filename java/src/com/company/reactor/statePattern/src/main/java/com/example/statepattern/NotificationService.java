8529cd8388b7aeddbf39166f9c8896ee145fcfed
haxul <s.s.starodubov@yandex.ru>
2022-02-12 17:12:57 +0400

    init

--- java/src/com/company/reactor/statePattern/src/main/java/com/example/statepattern/NotificationService.java
+++ java/src/com/company/reactor/statePattern/src/main/java/com/example/statepattern/NotificationService.java
@@ -1,11 +1,31 @@
 package com.example.statepattern;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 @Service
 public class NotificationService {
 
     public void sentMessage(Order order) {
         System.out.println("hello " + order.state.toString());
+    }
+
+    @Service
+    public static class ATM {
+
+        @Autowired
+        private ServicesMap servicesMap;
+
+        @PostConstruct
+        public void test() {
+            var order = new Order();
+            System.out.println(order.getState());
+            order.payOrder(servicesMap);
+            order.payTaxes(servicesMap);
+            order.payOrder(servicesMap);
+            order.payTaxes(servicesMap);
+        }
     }
 }
