11d7fd2bc1e141894d54536e674b39457b282244
haxul <s.s.starodubov@yandex.ru>
2022-03-21 19:41:35 +0400

    #1051 leetcode

--- java/src/com/company/leetcode/E1051/Main.java
+++ java/src/com/company/leetcode/E1051/Main.java
@@ -11,14 +11,31 @@ public class Main {
 
 class Solution {
     public int heightChecker(int[] heights) {
-        var cp = Arrays.copyOf(heights, heights.length);
-        Arrays.sort(cp);
-        int ans = 0;
-        for (int i = 0; i < heights.length; i++) {
-            if (heights[i]  != cp[i]) {
-                ans ++;
-            }
+        // counting sort
+
+        int[] counter = new int[101];
+
+        for (int height : heights) {
+            counter[height] += 1;
         }
+
+        for (int i = 1; i < counter.length; i++) {
+            counter[i] += counter[i - 1];
+        }
+
+        var sorted = new int[heights.length];
+
+        for (int h : heights) {
+            int idx = counter[h];
+            counter[h]--;
+            sorted[idx - 1] = h;
+        }
+        // wraping up
+        int ans = 0;
+        for (int i = 0; i < sorted.length; i++) {
+            if (sorted[i] != heights[i]) ans++;
+        }
+
         return ans;
     }
 }
