3bfa04ad44ad32ce5e3864c05baedb9d3eb8fab2
haxul <s.s.starodubov@yandex.ru>
2022-04-05 11:51:56 +0400

    #1475 leetcode

--- python/leetcode/1475. Final Prices With a Special Discount in a Shop.py
+++ python/leetcode/1475. Final Prices With a Special Discount in a Shop.py
@@ -10,22 +10,17 @@ class Solution:
             val = prices[i]
             while stack and stack[-1][1] >= val:
                 nidx, nval = stack.pop()
-                ans[nidx] = val
+                ans[nidx] = prices[nidx] - val
 
             stack.append((i, val))
 
         while stack:
-            si, sv =stack.pop()
-            ans[si] = -1
-        for i in range(n):
-            if ans[i] == -1:
-                ans[i] = prices[i]
-            else:
-                ans[i] = prices[i] - ans[i]
+            si, sv = stack.pop()
+            ans[si] = sv
+
         return ans
 
 
 assert Solution().finalPrices([4, 7, 1, 9, 4, 8, 8, 9, 4]) == [3, 6, 1, 5, 0, 0, 4, 5, 4]
-
 assert Solution().finalPrices([10, 1, 1, 6]) == [9, 0, 1, 6]
 assert Solution().finalPrices([8, 4, 6, 2, 3]) == [4, 2, 4, 2, 3]
