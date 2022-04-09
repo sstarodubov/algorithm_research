04e5b59453e19335bb5794657f806ecee4f8405a
haxul <s.s.starodubov@yandex.ru>
2022-04-09 10:49:36 +0400

    #98 leetcode

---
+++ python/leetcode/medium/98. Validate Binary Search Tree.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+from leetcode.tree_node import TreeNode
+
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def dfs(node: TreeNode, mx, mn):
+            if not node:
+                return True
+            if node.val >= mx:
+                return False
+            if node.val <= mn:
+                return False
+
+            return dfs(node.left, node.val, mn) and dfs(node.right, mx, node.val)
+
+        return dfs(root, float("inf"), float("-inf"))
+
+
+assert Solution().isValidBST(TreeNode.build_tree([2, 1, 3]))
