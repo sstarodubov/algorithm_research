9fcbc23d7012af4ab877df79b36724bcf63a9b55
haxul <s.s.starodubov@yandex.ru>
2022-04-13 23:34:10 +0400

    90 #leetcode

--- python/leetcode/medium/90. Subsets II.py
+++ python/leetcode/medium/90. Subsets II.py
@@ -3,33 +3,30 @@ from typing import List
 
 class Solution:
 
+    # time (n * 2 ^n)
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        ans = [[]]
+        ans = []
         nums.sort()
-        for idx, n in enumerate(nums):
+        def decision_tree(cur: List[int], idx):
+            if idx >= len(nums):
+                ans.append(cur)
+                return
 
-            tmp = []
-            for arr in ans:
-                tmp.append(arr[:])
+            rarr = cur[:]
+            larr = cur[:]
+            larr.append(nums[idx])
 
-            for arr in ans:
-                arr.append(n)
+            decision_tree(larr, idx + 1)
 
-            for arr in tmp:
-                ans.append(arr)
+            idx = idx + 1
+            while idx != 0 and idx < len(nums) and nums[idx - 1] == nums[idx]:
+                idx += 1
+            decision_tree(rarr, idx)
 
-        def key(ar: List[int]):
-            ar.sort()
-            return ''.join(str(e) for e in ar)
+        decision_tree([], 0)
 
-        used = set()
-        anss = []
-        for arr in ans:
-            k = key(arr)
-            if k not in used:
-                anss.append(arr)
-                used.add(k)
-        return anss
+        return ans
 
 
-print(Solution().subsetsWithDup([1, 2, 2]))
+print(Solution().subsetsWithDup([1, 1]))
+assert [[1, 2, 2], [1, 2], [1], [2, 2], [2], []] == Solution().subsetsWithDup([1, 2, 2])
