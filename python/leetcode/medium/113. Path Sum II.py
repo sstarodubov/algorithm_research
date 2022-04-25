9fa2ea09ebdafb314213f5bb52cf93e6c0fe267c
haxul <s.s.starodubov@yandex.ru>
2022-04-25 20:42:46 +0400

    #113 leetcode

---
+++ python/leetcode/medium/113. Path Sum II.py
@@ -0,0 +1,28 @@
+from typing import List, Optional
+
+from leetcode.tree_node import TreeNode
+
+
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        ans = []
+
+        def dfs(path: List[int], node: TreeNode):
+            if not node:
+                return
+            path.append(node.val)
+            if not node.left and not node.right:
+                if sum(path) == targetSum:
+                    ans.append(path[:])
+                path.pop()
+                return
+
+            dfs(path, node.left)
+            dfs(path, node.right)
+            path.pop()
+        dfs([], root)
+
+        return ans
+
+
+Solution().pathSum(root=TreeNode.build_tree([5, 4, 8, 11, None, 13, 4, 7, 2, None, None, 5, 1]), targetSum=22)
