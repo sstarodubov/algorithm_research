c772bcf8f7b4b9f62fa0ad351570b2ae1f751a66
haxul <s.s.starodubov@yandex.ru>
2022-05-23 20:59:21 +0400

    #1334 leetcode

---
+++ python/leetcode/1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+class Solution:
+    def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+        graph = []
+        for i in range(n):
+            arr = [float("inf")] * n
+            graph.append(arr)
+
+        for fr, to, w in edges:
+            graph[fr][to] = w
+            graph[to][fr] = w
+        distance = list(map(lambda i: list(map(lambda j: j, i)), graph))
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    if distance[i][k] + distance[k][j] < distance[i][j]:
+                        distance[i][j] = distance[i][k] + distance[k][j]
+
+        amount = float("inf")
+        city = -1
+        for i in range(n):
+            x = 0
+            for k in range(n):
+                if k == i:
+                    continue
+                v = distance[i][k]
+                if v <= distanceThreshold:
+                    x += 1
+
+            if x <= amount:
+                amount = x
+                city = i
+
+        return city
+
+
+assert 0 == Solution().findTheCity(n=5, edges=[[0, 1, 2], [0, 4, 8], [1, 2, 3], [1, 4, 2], [2, 3, 1], [3, 4, 1]],
+                                   distanceThreshold=2)
+assert 3 == Solution().findTheCity(n=4, edges=[[0, 1, 3], [1, 2, 1], [1, 3, 4], [2, 3, 1]], distanceThreshold=4)
