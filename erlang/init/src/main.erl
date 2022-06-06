265459c4da2f2967d9647fecf814f9e2a1808dbd
haxul <s.s.starodubov@yandex.ru>
2022-06-06 22:09:27 +0400

    erlang for fun

---
+++ erlang/init/src/main.erl
@@ -0,0 +1,24 @@
+-module(main).
+%%-export([main/0]).
+-compile(nowarn_export_all).
+-compile(export_all).
+
+main() -> 0.
+
+print([X | []]) -> print(X);
+print([Head | Tail]) ->
+  print(Head),
+  print(Tail);
+
+print(Num) when is_integer(Num) -> io:format(io_lib:format("~p ", [Num]));
+print(Str) when is_bitstring(Str) -> io:format(Str);
+print(Atom) when is_atom(Atom) -> io:format(atom_to_list(Atom));
+print(_) -> io:format("").
+
+
+head([H | _]) -> H.
+tail([_ | Tail]) -> Tail.
+
+greet(male, Name) -> io:format("Hello, Mr. ~s", [Name]);
+greet(female, Name) -> io:format("Hello, Mrs. ~s", [Name]);
+greet(_, Name) -> io:format("Hello, ~s", [Name]).
