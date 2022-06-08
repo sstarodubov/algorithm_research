642f93d08f84f43b2ff833fb4d6916c766fa73bd
haxul <s.s.starodubov@yandex.ru>
2022-06-08 22:52:25 +0400

    erlang for fun

--- erlang/init/src/main.erl
+++ erlang/init/src/main.erl
@@ -3,7 +3,26 @@
 -compile(nowarn_export_all).
 -compile(export_all).
 
-main() -> print(len([1,2,3,4])).
+main() -> (prepare_alarm("bath"))().
+
+
+prepare_alarm(Room) ->
+  io:format("~s is prepared", [Room]),
+  fun() -> io:format("~s alarms", [Room]) end.
+
+inc(X) -> X + 1.
+map(_, []) -> [];
+map(Fn, [H | T]) -> [Fn(H) | map(Fn, T)].
+
+compound(Fn, A, B) -> Fn(A, B).
+
+zip([], []) -> [];
+zip([X | XS], [Y | YS]) -> [{X, Y} | zip(XS, YS)].
+
+duplicate(El, Times) -> duplicate(El, Times, []).
+duplicate(_, 0, List) -> List;
+duplicate(El, Times, List) -> duplicate(El, Times - 1, [El | List]).
+
 
 len([], Acc) -> Acc;
 len([_ | Tail], Acc) -> len(Tail, Acc + 1).
@@ -14,15 +33,11 @@ print([Head | Tail]) ->
   print(Head),
   print(Tail);
 
+print(El) when is_tuple(El) -> print(tuple_to_list(El));
 print(Num) when is_integer(Num) -> io:format(integer_to_list(Num));
 print(Str) when is_bitstring(Str) -> io:format(Str);
 print(Atom) when is_atom(Atom) -> io:format(atom_to_list(Atom));
 print(_) -> io:format("").
 
-
 head([H | _]) -> H.
 tail([_ | Tail]) -> Tail.
-
-greet(male, Name) -> io:format("Hello, Mr. ~s", [Name]);
-greet(female, Name) -> io:format("Hello, Mrs. ~s", [Name]);
-greet(_, Name) -> io:format("Hello, ~s", [Name]).
