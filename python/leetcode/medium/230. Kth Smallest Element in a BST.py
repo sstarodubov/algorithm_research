1cb85917a8d4d5426a85b54d016e987635031556
haxul <s.s.starodubov@yandex.ru>
2022-06-13 18:48:41 +0400

    #230 leetcode

---
+++ python/leetcode/medium/230. Kth Smallest Element in a BST.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from leetcode.tree_node import TreeNode
+
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+
+        self.ans, self.c = -1, 0
+
+        def dfs(n: TreeNode):
+            if not n:
+                return
+
+            dfs(n.left)
+
+            if n.val != self.ans and self.c < k:
+                self.c += 1
+                self.ans = n.val
+
+            dfs(n.right)
+
+        dfs(root)
+
+        return self.ans
+
+assert Solution().kthSmallest(TreeNode.build_tree([3, 1, 4, None, 2]), k=1) == 1
+assert Solution().kthSmallest(TreeNode.build_tree([5, 3, 6, 2, 4, None, None, 1]), 3) == 3
