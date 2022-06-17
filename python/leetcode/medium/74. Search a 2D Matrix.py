f022f26aae8502fd305af37ff0f3ac64713977ea
haxul <s.s.starodubov@yandex.ru>
2022-06-17 20:18:02 +0400

    #leetcode #74

--- python/leetcode/medium/74. Search a 2D Matrix.py
+++ python/leetcode/medium/74. Search a 2D Matrix.py
@@ -1,10 +1,26 @@
 from typing import List
 
 
+# time O(n), space O(1)
 class Solution:
 
-    #time O(n), space O(1)
+    def binary_search(self, arr, target):
+        l, r = 0, len(arr) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+
+            if arr[m] == target:
+                return m
+            elif target < arr[m]:
+                r = m - 1
+            else:
+                l = m + 1
+
+        return -1
+
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
         r = len(matrix) - 1
         for row in range(len(matrix)):
             if matrix[row][0] < target:
@@ -14,16 +30,15 @@ class Solution:
             else:
                 r = row - 1
                 break
-        for col in range(len(matrix[0])):
-            if matrix[r][col] == target:
-                return True
+        idx = self.binary_search(matrix[r], target)
+        if idx == -1:
+            return False
+        return True
 
-        return False
 
-
-assert Solution().searchMatrix(matrix=[[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target=1)
 assert Solution().searchMatrix(matrix=[[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target=60)
 assert Solution().searchMatrix(matrix=[[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target=16)
+assert Solution().searchMatrix(matrix=[[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target=1)
 assert Solution().searchMatrix(matrix=[[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target=3)
 
 assert not Solution().searchMatrix(matrix=[[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target=13)
