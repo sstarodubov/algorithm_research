11f011f4c38adc12183f5b9cdaf5789a2ad030e1
haxul <s.s.starodubov@yandex.ru>
2022-06-20 23:37:11 +0400

    channels

--- java/src/com/company/javanio/Main.java
+++ java/src/com/company/javanio/Main.java
@@ -1,16 +1,18 @@
 package com.company.javanio;
 
 import java.io.*;
+import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.channels.WritableByteChannel;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -21,23 +23,34 @@ public class Main {
         var pathIn = "C:\\Users\\sssta\\development\\algorithm_tasks_solving\\java\\src\\com\\company\\javanio\\in.txt";
         var pathOut = "C:\\Users\\sssta\\development\\algorithm_tasks_solving\\java\\src\\com\\company\\javanio\\out.txt";
 
-        var in = Channels.newChannel(System.in);
-        var out = FileChannel.open(Path.of(pathOut), Set.of(StandardOpenOption.WRITE));
+        var sock = ServerSocketChannel.open();
+        sock.socket().bind(new InetSocketAddress(9999));
+        sock.configureBlocking(true);
+        var buf = ByteBuffer.wrap("hello from server".getBytes(StandardCharsets.UTF_8));
+        System.out.println(sock.socket().getLocalSocketAddress());
+        while (true) {
+            SocketChannel channel = sock.accept();
+            if (channel == null) {
+                System.out.println("channel is null");
+                return;
+            }
+            buf.rewind();
 
-        ByteBuffer buffer = ByteBuffer.allocateDirect(2048);
-        while (in.read(buffer) != 1) {
-            buffer.flip();
-            out.write(buffer);
-            out.force(false);
-            buffer.compact();
-        }
-
-        buffer.flip();
-        while (buffer.hasRemaining()) {
-            out.write(buffer);
+            channel.write(buf);
+            channel.close();
         }
     }
 
+    static void mapFile() throws IOException {
+        var pathIn = "C:\\Users\\sssta\\development\\algorithm_tasks_solving\\java\\src\\com\\company\\javanio\\in.txt";
+        var pathOut = "C:\\Users\\sssta\\development\\algorithm_tasks_solving\\java\\src\\com\\company\\javanio\\out.txt";
+        var buffer = CharBuffer.wrap("fuck you");
+        var channel = FileChannel.open(Path.of(pathOut), Set.of(StandardOpenOption.WRITE, StandardOpenOption.READ));
+        var mappedFileBuf = channel.map(FileChannel.MapMode.READ_WRITE, 0, buffer.length());
+        mappedFileBuf.put(StandardCharsets.UTF_8.encode(buffer));
+
+        System.out.println(Files.readAllLines(Path.of(pathOut)));
+    }
 
     static void copy(ReadableByteChannel src, WritableByteChannel dest) throws IOException {
         ByteBuffer buffer = ByteBuffer.allocateDirect(2048);
