503d3d89778e4aa2ca8c46ccbf7b024c0d1d277c
haxul <s.s.starodubov@yandex.ru>
2022-06-29 22:34:43 +0400

    #leetcode #322

--- python/leetcode/medium/322. Coin Change.py
+++ python/leetcode/medium/322. Coin Change.py
@@ -5,7 +5,7 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         if amount == 0:
             return 0
-        coins.sort(reverse=True)
+
         dp = [float("inf")] * (amount + 1)
         dp[0] = 0
 
