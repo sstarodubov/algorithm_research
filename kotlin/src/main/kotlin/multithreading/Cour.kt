9e66a6445490681e5ea8d2375390c70f294edbd0
haxul <s.s.starodubov@yandex.ru>
2022-07-18 23:41:23 +0400

    k

--- kotlin/src/main/kotlin/multithreading/Cour.kt
+++ kotlin/src/main/kotlin/multithreading/Cour.kt
@@ -4,9 +4,10 @@ import kotlinx.coroutines.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicInteger
+import kotlin.system.measureTimeMillis
 
-val eHandlerException = CoroutineExceptionHandler { coroutineContext, throwable ->
-    println(throwable.message)
+val eHandlerException = CoroutineExceptionHandler { _, _ ->
+    println("hoho")
 }
 
 
@@ -14,29 +15,57 @@ val executorService: ExecutorService = Executors.newCachedThreadPool()
 
 val scope = CoroutineScope(executorService.asCoroutineDispatcher())
 
-fun main() = runBlocking {
+suspend fun download(id: Int, time: Long, result: String, err: RuntimeException? = null): String {
+    try {
+        delay(time)
+        err?.let { throw err }
+        println("THREAD: ${Thread.currentThread().name}")
+        println(id)
+        return result
+    } catch (e:Exception ) {
+        print(e)
+        print("hoho")
+        println()
+        throw e
+    }
+}
 
-        scope.launch {
-            try {
-                Thread.sleep(1000)
-                throw RuntimeException("111")
-                println("here1: " + Thread.currentThread().name)
-            } catch (e: Exception) {
-                println("ERROR")
-                scope.coroutineContext.cancel()
-            }
+fun main(): Unit = runBlocking {
+    val time = measureTimeMillis {
+        val job2 = scope.async {
+            download(1, 5000, "world")
+        }
+        val job = scope.async {
+            download(2, 2000, "hello", RuntimeException("errorrrrrrrrrrr"))
         }
 
+        val ans = job.await() + job2.await()
+        println(ans)
+    }
+    println("time: $time")
 
-        scope.launch {
-            Thread.sleep(3000)
-            if (this.isActive) {
-                println("here2: " + Thread.currentThread().name)
-            }
-        }
-
-
-    println("here")
+    // =============================
+//        scope.launch {
+//            try {
+//                Thread.sleep(1000)
+//                throw RuntimeException("111")
+//                println("here1: " + Thread.currentThread().name)
+//            } catch (e: Exception) {
+//                println("ERROR")
+//                scope.coroutineContext.cancel()
+//            }
+//        }
+//
+//
+//        scope.launch {
+//            Thread.sleep(3000)
+//            if (this.isActive) {
+//                println("here2: " + Thread.currentThread().name)
+//            }
+//        }
+//
+//
+//    println("here")
 }
 
 
--- python/leetcode/medium/81. Search in Rotated Sorted Array II.py
+++ python/leetcode/medium/81. Search in Rotated Sorted Array II.py
@@ -3,8 +3,56 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        for n in nums:
-            if n == target:
-                return True
+        if len(nums) == 1: return nums[0] == target
 
+        def find_pivot():
+            if nums[0] < nums[-1]: return 0
+
+            l, r = 0, len(nums) - 1
+
+            while l <= r:
+                m = l + (r - l) // 2
+
+                if nums[m] > nums[m + 1]:
+                    return m + 1
+
+                if nums[m] > nums[-1]:
+                    l = m + 1
+
+                else:
+                    r = m
+            return -1
+
+        def bs(l, r):
+
+            while l <= r:
+
+                m = l + (r - l) // 2
+
+                if nums[m] == target:
+                    return True
+
+                if nums[m] < target:
+                    l = m + 1
+                else:
+                    r = m - 1
+
+            return False
+
+
+        pidx = find_pivot()
+
+        if pidx == 0:
+            return bs(0, len(nums) - 1)
+
+        x = bs(0, pidx - 1)
+        y = bs(pidx, len(nums) - 1)
+
+        if x: return True
+        if y: return True
         return False
+
+
+assert not Solution().search(nums=[2, 5, 6, 0, 0, 1, 2], target=3)
+assert Solution().search([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1], 2);
+assert Solution().search(nums=[2, 5, 6, 0, 0, 1, 2], target=0)
