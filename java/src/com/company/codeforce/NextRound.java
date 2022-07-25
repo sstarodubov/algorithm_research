dd24719e43d8af8f5710afc96e2df77808a17bf6
haxul <s.s.starodubov@yandex.ru>
2022-07-25 18:18:53 +0400

    #codeforce 158A

---
+++ java/src/com/company/codeforce/NextRound.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class NextRound {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int ans = 0;
+
+        for (int score : arr) {
+            if (score != 0 && score >= arr[k - 1]) {
+                ans++;
+            }
+        }
+        System.out.println(ans);
+    }
+}
