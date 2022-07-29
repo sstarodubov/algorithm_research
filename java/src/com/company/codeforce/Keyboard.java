0f62d7df796ec93a02f037d3728ec36574ce733e
haxul <s.s.starodubov@yandex.ru>
2022-07-29 21:21:25 +0400

    #codeforce

---
+++ java/src/com/company/codeforce/Keyboard.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Keyboard {
+    private final static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+        var keybord = "qwertyuiopasdfghjkl;zxcvbnm,./";
+
+        var im = new HashMap<Integer, Character>();
+        var cm = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < keybord.length(); i++) {
+            im.put(i, keybord.charAt(i));
+            cm.put(keybord.charAt(i), i);
+        }
+
+        var d = sc.next();
+        var s = sc.next();
+
+        var ans = new StringBuilder();
+
+        if (d.equals("R")) {
+            for (int i = 0; i < s.length(); i++) {
+                var typed = s.charAt(i);
+                var wanted = (im.get(cm.get(typed) - 1));
+                ans.append(wanted);
+            }
+        } else {
+            for (int i = 0; i < s.length(); i++) {
+                var typed = s.charAt(i);
+                var wanted = (im.get(cm.get(typed) + 1));
+                ans.append(wanted);
+            }
+        }
+        System.out.println(ans);
+    }
+}
