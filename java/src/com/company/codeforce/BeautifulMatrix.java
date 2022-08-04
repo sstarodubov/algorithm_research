678f64821ff2662970a2f9d9da19264145e40d1d
haxul <s.s.starodubov@yandex.ru>
2022-08-04 21:36:50 +0400

    codeforces

---
+++ java/src/com/company/codeforce/BeautifulMatrix.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class BeautifulMatrix {
+    public static void main(String[] args) {
+        final var sc = new Scanner(System.in);
+        int r = -1;
+        int c = -1;
+        for (int row = 0; row < 5; row++) {
+            for (int col = 0; col < 5; col++) {
+                int n = sc.nextInt();
+                if (n == 1) {
+                    r = row;
+                    c = col;
+                }
+            }
+        }
+
+        int dr = 0, dc = 0;
+
+        if (r > 2) {
+            dr = r - 2;
+        } else if (r < 2) {
+            dr = 2 - r;
+        }
+
+        if (c > 2) {
+            dc = c - 2;
+        } else if (c < 2) {
+            dc = 2 - c;
+        }
+
+        System.out.println(dc + dr);
+    }
+}
---
+++ java/src/com/company/codeforce/SoldierandBananas.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SoldierandBananas {
+    public static void main(String[] args) {
+        final var sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int n = sc.nextInt();
+        int w = sc.nextInt();
+
+        int s = (((w * (w + 1)) * k)) / 2;
+
+        int ans = s - n;
+        if (ans < 0) {
+            System.out.println(0);
+        } else {
+            System.out.println(ans);
+        }
+    }
+}
