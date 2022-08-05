68597ea38383aeffc206f881569effa020a8d169
haxul <s.s.starodubov@yandex.ru>
2022-08-05 21:44:13 +0400

    codeforces

---
+++ java/src/com/company/codeforce/Translation.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Translation {
+
+    public static void main(String[] args) {
+        final var sc = new Scanner(System.in);
+        var s = sc.next();
+        var r = sc.next();
+
+        if (s.length() != r.length()) {
+            System.out.println("NO");
+            return;
+        }
+
+        for (int i = 0, j = s.length() - 1; i < s.length(); i++, j--) {
+            if (s.charAt(i) != r.charAt(j)) {
+                System.out.println("NO");
+                return;
+            }
+        }
+
+        System.out.println("YES");
+    }
+}
