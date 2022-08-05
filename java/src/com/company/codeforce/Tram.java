4e5246ce39275c5ad3d0fdbf9627664fa404062a
haxul <s.s.starodubov@yandex.ru>
2022-08-05 21:35:02 +0400

    codeforces

---
+++ java/src/com/company/codeforce/Tram.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Tram {
+
+    public static void main(String[] args) {
+        final var sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cap = 0;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int out = sc.nextInt();
+            int in = sc.nextInt();
+
+            cap = cap - out + in;
+            ans = Math.max(ans, cap);
+        }
+
+        System.out.println(ans);
+    }
+}
