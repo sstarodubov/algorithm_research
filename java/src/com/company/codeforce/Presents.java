d8e012effaa2c28340f833fc5a60a8a825e231ed
haxul <s.s.starodubov@yandex.ru>
2022-08-08 20:34:22 +0400

    codeforces

---
+++ java/src/com/company/codeforce/Presents.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Presents {
+
+    public static void main(String[] args) {
+        final var sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[sc.nextInt() - 1] = i + 1;
+        }
+
+        displayArr(arr);
+    }
+
+    private static void displayArr(int[] arr) {
+        final var sb = new StringBuilder();
+        for (int j : arr) {
+            sb.append(j).append(" ");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+
+        System.out.println(sb);
+    }
+}
