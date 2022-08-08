29295e81759c3c01b41cf4ff9061c5cabc29c253
haxul <s.s.starodubov@yandex.ru>
2022-08-08 20:10:47 +0400

    codeforces

---
+++ java/src/com/company/codeforce/VanyaAndFence.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class VanyaAndFence {
+
+    public static void main(String[] args) {
+        final var sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int h = sc.nextInt();
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (sc.nextInt() > h) {
+                ans += 2;
+            } else {
+                ans += 1;
+            }
+        }
+
+        System.out.println(ans);
+
+    }
+}
