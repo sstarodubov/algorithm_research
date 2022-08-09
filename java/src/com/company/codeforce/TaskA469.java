59e34442ee3057e8f618cbc25ea555f5b34973a1
haxul <s.s.starodubov@yandex.ru>
2022-08-09 20:49:41 +0400

    codeforces

---
+++ java/src/com/company/codeforce/TaskA469.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class TaskA469 {
+
+    public static void main(String[] args) {
+        final var sc = new Scanner(System.in);
+        var set = new HashSet<Integer>();
+
+        int n = sc.nextInt();
+
+        int xn = sc.nextInt();
+
+        for (int i = 0; i < xn; i++) {
+            set.add(sc.nextInt());
+        }
+
+        int yn = sc.nextInt();
+
+        for (int i = 0; i < yn; i++) {
+            set.add(sc.nextInt());
+        }
+
+
+        if (set.size() == n) {
+            System.out.println("I become the guy.");
+        } else {
+            System.out.println("Oh, my keyboard!");
+        }
+    }
+}
