eb307d33102b3310122c398847eb6676975ab95b
haxul <s.s.starodubov@yandex.ru>
2022-08-09 20:15:03 +0400

    codeforces

---
+++ java/src/com/company/codeforce/UltraFastMathematician.java
@@ -0,0 +1,17 @@
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class UltraFastMathematician {
+
+    public static void main(String[] args) {
+        final var sc = new Scanner(System.in);
+        var n = sc.next();
+        var n1 = new BigInteger(n, 2);
+        var n2 = sc.nextBigInteger(2);
+
+        System.out.println(
+                String.format("%" + n.length() + "s", n2.xor(n1).toString(2)).replace(' ', '0')
+        );
+    }
+}
