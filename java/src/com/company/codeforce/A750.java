d9a4effc25686c79bc3fd24b0cf91e02f9bae528
haxul <s.s.starodubov@yandex.ru>
2022-08-12 20:39:14 +0400

    codeforces

---
+++ java/src/com/company/codeforce/A750.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class A750 {
+
+    public static void main(String[] args) {
+        final var sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int left = 240 - k;
+
+        int ans = 0;
+        int c = 0;
+        for (int i = 1; i <= n; i++) {
+            if ((ans + (5 * i)) > left) {
+                System.out.println(c);
+                return;
+            }
+            c++;
+            ans += (5 * i);
+        }
+
+        System.out.println(c);
+    }
+}
