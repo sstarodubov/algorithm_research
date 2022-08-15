3534aafcd65fdab2795dfcc4a9626d4ddafe7ec8
haxul <s.s.starodubov@yandex.ru>
2022-08-15 19:00:15 +0400

    #codeforce

---
+++ java/src/com/company/codeforce/A1409.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class A1409 {
+
+    public static void main(String[] args) throws Exception {
+        final Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            long diff = Math.abs(a - b);
+            System.out.println((diff / 10) + ((diff % 10) == 0 ? 0 : 1));
+        }
+    }
+
+    public static int[] read(int size, final Scanner sc) {
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+}
