feca56c3bd03498dfb1f717ef22fb7e6dccbe590
haxul <s.s.starodubov@yandex.ru>
2022-08-23 20:58:28 +0400

    #codeforce

---
+++ java/src/com/company/codeforce/A381.java
@@ -0,0 +1,41 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class A381 {
+
+    public static void main(String[] args) throws Exception {
+        final Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        var dq = new ArrayDeque<Integer>();
+        for (int i = 0; i < n; i++) {
+            dq.add(sc.nextInt());
+        }
+        int s = 0;
+        int d = 0;
+        boolean isSm = true;
+
+        while (!dq.isEmpty()) {
+            Integer first = dq.peekFirst();
+            Integer last = dq.peekLast();
+
+            if (isSm) {
+                if (first > last) {
+                    s += dq.pollFirst();
+                } else {
+                    s += dq.pollLast();
+                }
+                isSm = false;
+            } else {
+
+                if (first > last) {
+                    d += dq.pollFirst();
+                } else {
+                    d += dq.pollLast();
+                }
+                isSm = true;
+            }
+        }
+
+        System.out.printf("%d %d", s, d);
+    }
+}
