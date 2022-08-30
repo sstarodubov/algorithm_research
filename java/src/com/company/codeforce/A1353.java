569a26d6d5d66624c6e5d4218c29abe0aa691ae2
haxul <s.s.starodubov@yandex.ru>
2022-08-30 23:12:05 +0400

    #codeforce

---
+++ java/src/com/company/codeforce/A1353.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class A1353 {
+
+    public static void main(String[] args) throws Exception {
+        final Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            long n = sc.nextLong();
+            long m = sc.nextLong();
+            helper(n, m);
+        }
+    }
+
+    private static void helper(long n, long m) {
+        if (n == 1) {
+            System.out.println(0);
+            return;
+        }
+        if (n == 2) {
+            System.out.println(m);
+            return;
+        }
+
+        System.out.println(m * 2);
+    }
+}
---
+++ java/src/com/company/codeforce/B1520.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class B1520 {
+
+    public static void main(String[] args) throws Exception {
+        final Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            long n = sc.nextLong();
+            helper(n);
+        }
+    }
+
+    private static void helper(long n) {
+        long size = 0;
+        long mask = 0;
+        long t = n;
+        while (n != 0) {
+            n /= 10;
+            mask += Math.pow(10, size);
+            size++;
+        }
+        long fDig = t < 10 ? t : (long) (t / (Math.pow(10, size - 1)));
+        if (t < 10) {
+            System.out.println(t);
+        } else {
+            long p = mask * fDig;
+            var x = (size - 1) * 9;
+            var add = p % 10;
+            var result = x + add;
+            if (t < p) {
+                result--;
+            }
+            System.out.println(result);
+        }
+
+    }
+}
