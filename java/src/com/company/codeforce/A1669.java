36a64d36eb287da843d2753915b9b11702fe20e1
haxul <s.s.starodubov@yandex.ru>
2022-09-06 20:11:22 +0400

    #codeforce

--- java/src/com/company/codeforce/A1669.java
+++ java/src/com/company/codeforce/A1669.java
@@ -4,29 +4,19 @@ public class A1669 {
 
     public static void main(String[] args) throws Exception {
         final Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
 
-    }
-
-    public static void print(int[] arr) {
-        var s = new StringBuilder();
-        for (int j : arr) {
-            s.append(j).append(" ");
+        for (int i = 0; i < t; i++) {
+            int x = sc.nextInt();
+            if (x <= 1399) {
+                System.out.println("Division " + 4);
+            } else if (x >= 1400 && x <= 1599) {
+                System.out.println("Division " + 3);
+            } else if (x >= 1600 && x <= 1899) {
+                System.out.println("Division " + 2);
+            } else {
+                System.out.println("Division " + 1);
+            }
         }
-        s.deleteCharAt(s.length() - 1);
-        System.out.println(s);
-    }
-
-    public static int[] read(int size, final Scanner sc) {
-        int[] arr = new int[size];
-        for (int i = 0; i < size; i++) {
-            arr[i] = sc.nextInt();
-        }
-        return arr;
-    }
-
-    public static void swap(int[] arr, int i, int j) {
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
     }
 }
