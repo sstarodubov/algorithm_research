ae9ccd8308aab6b711108f8b9e1b422548634eb3
haxul <s.s.starodubov@yandex.ru>
2022-09-09 21:01:00 +0400

    #codeforce

---
+++ java/src/com/company/codeforce/A233.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class A233 {
+
+    public static void main(String[] args) throws Exception {
+        final Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if ((n & 1) == 1) {
+            System.out.println(-1);
+            return;
+        }
+
+        var arr = new int[n];
+        for (int i = 1; i <= n; i++) {
+            arr[i - 1] = i;
+        }
+
+        for (int i = 0; i < n - 1; i += 2) {
+            swap(arr, i, i + 1);
+        }
+
+        print(arr);
+    }
+
+
+    public static void print(int[] arr) {
+        var s = new StringBuilder();
+        for (int j : arr) {
+            s.append(j).append(" ");
+        }
+        s.deleteCharAt(s.length() - 1);
+        System.out.println(s);
+    }
+
+    public static int[] read(int size, final Scanner sc) {
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
