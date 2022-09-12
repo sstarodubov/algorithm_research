99a1c7071da5bf10b6202a51870ed8a8803c9ea5
haxul <s.s.starodubov@yandex.ru>
2022-09-12 20:54:51 +0400

    #codeforce

---
+++ java/src/com/company/codeforce/B1703.java
@@ -0,0 +1,63 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class B1703 {
+
+    public static void main(String[] args) throws Exception {
+        final Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            fn(sc);
+        }
+    }
+
+    private static void fn(Scanner sc) {
+        int n = sc.nextInt();
+        var s = sc.next();
+        int ans = 0;
+        var set = new HashSet<Character>();
+        for (int i = 0; i < n; i++) {
+            if (set.contains(s.charAt(i))) {
+                ans++;
+            } else {
+                ans += 2;
+                set.add(s.charAt(i));
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    static int greaterCommonDeviser(int a, int b) {
+        int R;
+        while ((a % b) > 0) {
+            R = a % b;
+            a = b;
+            b = R;
+        }
+        return b;
+    }
+
+    public static void print(int[] arr) {
+        var s = new StringBuilder();
+        for (int j : arr) {
+            s.append(j).append(" ");
+        }
+        s.deleteCharAt(s.length() - 1);
+        System.out.println(s);
+    }
+
+    public static int[] read(int size, final Scanner sc) {
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
