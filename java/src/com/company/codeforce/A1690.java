3a74edfceb71bc9254727a718b3defff4d72c8ae
haxul <s.s.starodubov@yandex.ru>
2022-09-16 21:30:27 +0400

    #codeforce

---
+++ java/src/com/company/codeforce/A1690.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class A1690 {
+
+    public static void main(String[] args) throws Exception {
+        final Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            fn(sc);
+        }
+    }
+
+    private static void fn(Scanner sc) {
+        int n = sc.nextInt();
+        int d = n / 3;
+        int r = n % 3;
+        int h1, h2, h3;
+        if (r == 2) {
+            h1 = d + 2;
+            h2 = d + 1;
+            h3 = d - 1;
+        } else if (r == 1) {
+            h1 = d + 2;
+            h2 = d;
+            h3 = d - 1;
+        } else {
+            h1 = d + 1;
+            h2 = d;
+            h3 = d - 1;
+        }
+
+        System.out.printf("%d %d %d\n", h2, h1, h3);
+    }
+}
