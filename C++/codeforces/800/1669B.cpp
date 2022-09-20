0aaa542d0616179e7b41ad8eaf9481e683c3a661
haxul <s.s.starodubov@yandex.ru>
2022-09-20 23:11:55 +0400

    codeforces

---
+++ C++/codeforces/800/1669B.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+
+std::map<int, int> m;
+
+void solve() {
+
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int j = 0; j < n; ++j) {
+        int w;
+        cin >> w;
+        arr[j] = w;
+    }
+    if (n == 9) {
+        int a = 0;
+    }
+    for (int i = 0; i < n; ++i) {
+        int p = arr[i];
+        if (m.find(p) != m.end()) {
+            m[p] = m[p] + 1;
+            if (m[p] == 3) {
+                cout << p << endl;
+                m.clear();
+                return;
+            }
+        } else {
+            m[p] = 1;
+        }
+    }
+
+    cout << -1 << endl;
+    m.clear();
+}
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; ++i) {
+        solve();
+    }
+    return 0;
+}
---
+++ C++/codeforces/copy.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+read -r filename;
+cp main.cpp "800/$filename.cpp"
