8227bfe9eb7cb25fdc4c8b1464b6cbd51b6a390a
haxul <s.s.starodubov@yandex.ru>
2022-09-23 21:10:19 +0400

    algo

--- java/src/com/company/courera/courseera/graph/DiGraph.java
+++ java/src/com/company/courera/courseera/graph/DiGraph.java
@@ -5,39 +5,48 @@ import java.util.*;
 public class DiGraph implements Graph {
 
     public static void main(String[] args) {
-        DiGraph diGraph = new DiGraph(13);
-
+        DiGraph diGraph = new DiGraph(7);
         diGraph.addEdge(0, 1);
         diGraph.addEdge(0, 5);
-        diGraph.addEdge(2, 0);
-        diGraph.addEdge(2, 3);
-        diGraph.addEdge(3, 5);
+        diGraph.addEdge(0, 2);
+        diGraph.addEdge(1, 4);
         diGraph.addEdge(3, 2);
-        diGraph.addEdge(4, 2);
-        diGraph.addEdge(4, 3);
-        diGraph.addEdge(5, 4);
+        diGraph.addEdge(3, 5);
+        diGraph.addEdge(3, 6);
+        diGraph.addEdge(3, 4);
+        diGraph.addEdge(5, 2);
         diGraph.addEdge(6, 0);
         diGraph.addEdge(6, 4);
-        diGraph.addEdge(6, 9);
-        diGraph.addEdge(6, 8);
-        diGraph.addEdge(7, 6);
-        diGraph.addEdge(7, 9);
-        diGraph.addEdge(8, 6);
-        diGraph.addEdge(9, 10);
-        diGraph.addEdge(9, 11);
-        diGraph.addEdge(10, 12);
-        diGraph.addEdge(11, 12);
-        diGraph.addEdge(11, 4);
-        diGraph.addEdge(12, 9);
 
-        diGraph.bfs(12, 0);
+        diGraph.topologicalSort();
     }
 
     private final int V;
     private final List<List<Integer>> adj;
     private int E = 0;
 
-    private record Pair(int prev, int cur) {}
+    public void topologicalSort() {
+        var topSort = new ArrayList<Integer>();
+        var visited = new HashSet<Integer>();
+        for (int n = 0; n < V; n++) {
+            topologicalSort(n, topSort, visited);
+        }
+        Collections.reverse(topSort);
+        System.out.println(topSort);
+    }
+
+    private void topologicalSort(int w, List<Integer> sorted, Set<Integer> visited) {
+        if (visited.contains(w)) return;
+        visited.add(w);
+        var nList = this.adj.get(w);
+        for (int n : nList) {
+            topologicalSort(n, sorted, visited);
+        }
+        sorted.add(w);
+    }
+
+    private record Pair(int prev, int cur) {
+    }
 
     public void bfs(int fr, int to) {
         int[] path = new int[V];
