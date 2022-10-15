e1e6fadb85e15ecb1df4f1241b3942e1d9172900
haxul <s.s.starodubov@yandex.ru>
2022-10-15 18:12:18 +0400

    asm

---
+++ C++/algo_section/algo_section/algo_section/addsumfun.asm
@@ -0,0 +1,17 @@
+
+; int IntegerAddSub_(int a, int b, int c)
+
+; ecx = a, edx = b , r8d = c
+; return eax
+
+    .code
+IntegerAddSub_ proc
+	
+	mov eax,ecx		;eax = a
+	add eax,edx		;eax = a + b
+	sub eax,r8d		;eax = a + b - c
+	
+	ret
+
+IntegerAddSub_ endp
+	end
