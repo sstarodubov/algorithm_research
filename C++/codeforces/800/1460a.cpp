f3c67fdd9c62bd262cc8e220a58c5cdba213900e
haxul <s.s.starodubov@yandex.ru>
2022-11-29 19:24:03 +0400

    codeforces

---
+++ C++/codeforces/800/1460a.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+
+bool contains(unordered_set<int> &set, int &val) {
+    return set.find(val) != set.end();
+}
+
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef long long ll;
+
+void solve();
+
+void print_arr(int n, int *arr) {
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+
+        if (i == n - 1) {
+            cout << endl;
+        }
+    }
+}
+
+void read_arr(int n, int *a) {
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+}
+
+int main() {
+    int t;
+
+    cin >> t;
+
+    for (int i = 0; i < t; ++i) {
+        solve();
+    }
+
+    return 0;
+}
+
+void solve() {
+    int arr[3];
+
+    read_arr(3, arr);
+
+    sort(begin(arr), end(arr));
+
+    cout << arr[1] << endl;
+}
