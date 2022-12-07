2ecba4ed384d129845204557f2a2f6e932b8af22
haxul <s.s.starodubov@yandex.ru>
2022-12-07 19:44:33 +0400

    codeforces

---
+++ C++/codeforces/800/1519A.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <array>
+#include <cmath>
+
+using namespace std;
+
+bool contains(unordered_set<int> &set, int &val) {
+    return set.find(val) != set.end();
+}
+
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef long long ll;
+
+void solve();
+
+void print_arr(int n, int *arr) {
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+
+        if (i == n - 1) {
+            cout << endl;
+        }
+    }
+}
+
+void read_arr(int n, int *a) {
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+}
+
+int main() {
+    int t;
+
+    cin >> t;
+
+    for (int i = 0; i < t; ++i) {
+        solve();
+    }
+
+    return 0;
+}
+
+void solve() {
+    double r, b, d;
+    cin >> r;
+    cin >> b;
+    cin >> d;
+
+    double mn = min(r, b);
+    double mx = max(r, b);
+
+    auto c = ceil(mx / mn);
+    if ((c - 1) <= d) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
