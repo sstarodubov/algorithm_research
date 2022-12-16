c211a19c4742919b63a09406995f474dc7c96269
haxul <s.s.starodubov@yandex.ru>
2022-12-16 20:48:34 +0400

    #leetcode

--- python/leetcode/maraphon/236. Lowest Common Ancestor of a Binary Tree.py
+++ python/leetcode/maraphon/236. Lowest Common Ancestor of a Binary Tree.py
@@ -2,38 +2,35 @@ from leetcode.tree_node import TreeNode
 
 
 class Solution:
+
+    def path(self, n, arr, t):
+        if not n:
+            return False
+        arr.append(n)
+        if n == t:
+            return True
+        f = self.path(n.left, arr, t)
+        h = False
+        if not f:
+            h = self.path(n.right, arr, t)
+        if not h and not f:
+            arr.pop()
+        return h or f
+
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if not root or not p or not q:
             return None
 
-        def path(n, arr, t):
-            if not n:
-                return False
-            arr.append(n)
-            if n == t:
-                return True
-            f = path(n.left, arr, t)
-            h = False
-            if not f:
-                h = path(n.right, arr, t)
-            if not h and not f:
-                arr.pop()
-            return h or f
+        pp, qp = [], []
 
-        pp = []
-        path(root, pp, p)
-
-        qp = []
-        path(root, qp, q)
+        self.path(root, pp, p)
+        self.path(root, qp, q)
 
         i = 0
         while i < len(pp) and i < len(qp):
             if pp[i] != qp[i]:
                 return pp[i - 1]
             i += 1
-
-        if i == 0:
-            return None
 
         return pp[i - 1]
 
