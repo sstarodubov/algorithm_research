5b496b8e18e8f036a4b874368aebcc0b36cb8c6e
haxul <s.s.starodubov@yandex.ru>
2023-01-18 21:09:37 +0400

    #leetcode

---
+++ python/leetcode/trees/543. Diameter of Binary Tree.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from leetcode.tree_node import TreeNode
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.ans = 0
+
+        def dfs(n: TreeNode):
+            if not n:
+                return 0
+
+            lv = dfs(n.left)
+            rv = dfs(n.right)
+
+            self.ans = max(self.ans, lv + rv)
+
+            return max(lv, rv) + 1
+
+        dfs(root)
+
+        return self.ans
+
+
+assert 1 == Solution().diameterOfBinaryTree(TreeNode.build_tree([1, 2]))
+assert 3 == Solution().diameterOfBinaryTree(TreeNode.build_tree([1, 2, 3, 4, 5]))
---
+++ python/leetcode/trees/563. Binary Tree Tilt.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+from leetcode.tree_node import TreeNode
+
+
+class Solution:
+    def findTilt(self, root: Optional[TreeNode]) -> int:
+        self.ans = 0
+
+        def dfs(n: TreeNode):
+            if not n:
+                return 0
+
+            if not n.left and not n.right:
+                return n.val
+
+            lv = dfs(n.left)
+            rv = dfs(n.right)
+
+            t = abs(lv - rv)
+            self.ans += t
+
+            return lv + rv + n.val
+
+        dfs(root)
+
+        return self.ans
+
+
+assert 15 == Solution().findTilt(TreeNode.build_tree([4, 2, 9, 3, 5, None, 7]))
+assert 1 == Solution().findTilt(TreeNode.build_tree([1, 2, 3]))
