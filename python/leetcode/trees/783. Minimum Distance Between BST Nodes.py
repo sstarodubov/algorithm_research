71a5bfce5a92c149edb3f00434ef7131cc01c4db
haxul <s.s.starodubov@yandex.ru>
2023-01-23 22:40:41 +0400

    #leetcode

---
+++ python/leetcode/trees/783. Minimum Distance Between BST Nodes.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+from leetcode.tree_node import TreeNode
+
+
+class Solution:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        self.ans = float("inf")
+        self.prev = None
+
+        def dfs(n: TreeNode):
+            if not n:
+                return
+
+            dfs(n.left)
+            if self.prev is None:
+                self.prev = n.val
+            else:
+                self.ans = min(abs(n.val - self.prev), self.ans)
+                self.prev = n.val
+            dfs(n.right)
+
+        dfs(root)
+
+        return self.ans
