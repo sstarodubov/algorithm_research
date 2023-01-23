64a4bb95deaa52b685f391744229b355aca85327
haxul <s.s.starodubov@yandex.ru>
2023-01-23 23:06:26 +0400

    #leetcode

---
+++ python/leetcode/trees/938. Range Sum of BST.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from leetcode.tree_node import TreeNode
+
+
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        self.ans = 0
+
+        def dfs(n: TreeNode):
+            if not n:
+                return
+
+            if n.val > low:
+                dfs(n.left)
+            if low <= n.val <= high:
+                self.ans += n.val
+            if n.val < high:
+                dfs(n.right)
+
+        dfs(root)
+        return self.ans
