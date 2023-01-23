7d2139b36ceb2d329542687baab1fe6727356d2e
haxul <s.s.starodubov@yandex.ru>
2023-01-23 23:02:47 +0400

    #leetcode

---
+++ python/leetcode/trees/897. Increasing Order Search Tree.py
@@ -0,0 +1,30 @@
+from leetcode.tree_node import TreeNode
+
+
+class Solution:
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        self.prev = None
+        self.ans = None
+
+        def dfs(n: TreeNode):
+            if not n:
+                return
+
+            dfs(n.left)
+            if self.prev is None:
+                self.prev = n
+                self.ans = n
+            else:
+                self.prev.left = None
+                self.prev.right = n
+                self.prev = n
+
+            dfs(n.right)
+
+        dfs(root)
+        self.prev.left = None
+        return self.ans
+
+
+reulst = Solution().increasingBST(TreeNode.build_tree([2, 1, 4, None, None, 3]))
+print(reulst)
