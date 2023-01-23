be909c5101f772d89867f212b2c2ca581271ea64
haxul <s.s.starodubov@yandex.ru>
2023-01-23 22:44:43 +0400

    #leetcode

---
+++ python/leetcode/trees/872. Leaf-Similar Trees.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+from leetcode.tree_node import TreeNode
+
+
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+
+        l1 = []
+        l2 = []
+
+        def dfs(n: TreeNode, l):
+            if not n:
+                return
+            if not n.left and not n.right:
+                l.append(n.val)
+                return
+
+            dfs(n.left, l)
+            dfs(n.right, l)
+
+        dfs(root1, l1)
+        dfs(root2, l2)
+
+        return l1 == l2
