d0ff5dc040bdda06d4661b1da88fb44bbf67473b
haxul <s.s.starodubov@yandex.ru>
2023-12-22 18:26:41 +0400

    leetcode

--- python/leetcode/maraphon/380. Insert Delete GetRandom O(1).py
+++ python/leetcode/maraphon/380. Insert Delete GetRandom O(1).py
@@ -4,32 +4,31 @@ import random
 class RandomizedSet:
 
     def __init__(self):
-        self.set = set()
+        self.m = {}
+        self.xs = []
 
     def insert(self, val: int) -> bool:
-        if val in self.set:
-            self.set.add(val)
-            return False
-        else:
-            self.set.add(val)
-            return True
+        ret = val not in self.m
+        if ret:
+            self.m[val] = len(self.xs)
+            self.xs.append(val)
+        return ret
 
     def remove(self, val: int) -> bool:
-        if val in self.set:
-            self.set.remove(val)
-            return True
-        else:
-            return False
+        ret: bool = val in self.m
+        if ret:
+            i = self.m[val]
+            last = self.xs[-1]
+            self.xs[i] = last
+            self.xs.pop()
+            self.m[last] = i
+            del self.m[val]
+        return ret
 
     def getRandom(self) -> int:
-        l = len(self.set)
+        l = len(self.xs)
         r = random.randint(0, l - 1)
-        c = 0
-        for v in self.set:
-            if c == r:
-                return v
-            c += 1
-        return -1
+        return self.xs[r]
 
 
 s = RandomizedSet()
