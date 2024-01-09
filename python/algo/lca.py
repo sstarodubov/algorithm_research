07aa5c37273fe53ac32f3f55ff7929d29bc72f5b
haxul <s.s.starodubov@yandex.ru>
2024-01-09 23:16:05 +0400

    algo

--- python/algo/lca.py
+++ python/algo/lca.py
@@ -5,14 +5,12 @@ from leetcode.tree_node import TreeNode
 tree = TreeNode.build_tree([3, 6, 8, 2, 4, None, 13, None, None, 9, 5, 7])
 
 
-def lca(node: Optional[TreeNode], n1: int, n2: int) -> Optional[TreeNode]:
-    if not node: return None
-    if node.val == n1 or node.val == n2: return node
-    left: Optional[TreeNode] = lca(node.left, n1, n2)
-    right: Optional[TreeNode] = lca(node.right, n1, n2)
-    if left and right: return node
-    if not left and not right: return None
-    return right if right else left
-
-
-print(lca(tree, 9, 5).val)
+class Solution:
+    def lowestCommonAncestor(self, node: Optional['TreeNode'], p: 'TreeNode', q: 'TreeNode') -> Optional['TreeNode']:
+        if not node: return None
+        if node.val == p.val or node.val == q.val: return node
+        left: Optional[TreeNode] = self.lowestCommonAncestor(node.left, p, q)
+        right: Optional[TreeNode] = self.lowestCommonAncestor(node.right, p, q)
+        if left and right: return node
+        if not left and not right: return None
+        return right if right else left
