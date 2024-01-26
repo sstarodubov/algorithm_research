c56a4997e3e8514c2bbba6478c61012874f25bf5
haxul <s.s.starodubov@yandex.ru>
2024-01-26 22:43:26 +0400

    algo

---
+++ python/leetcode/maraphon/199. Binary Tree Right Side View.py
@@ -0,0 +1,32 @@
+from typing import Optional, List
+from collections import deque
+
+from leetcode.tree_node import TreeNode
+
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        res = []
+        prevp = (-1, TreeNode(-101))
+        dq = deque()
+        dq.append((0, root))
+
+        while dq:
+            dep, cur = dq.popleft()
+            pdep, prev = prevp
+            if dep != pdep and prev.val != -101:
+                res.append(prev.val)
+            prevp = (dep, cur)
+            if cur.left:
+                dq.append((dep + 1, cur.left))
+            if cur.right:
+                dq.append((dep + 1, cur.right))
+        res.append(prevp[1].val)
+        return res
+
+
+print(Solution().rightSideView(TreeNode.build_tree([1, 2, 3, None, 5, None, 4])))
+print(Solution().rightSideView(TreeNode.build_tree([1, None, 3])))
+print(Solution().rightSideView(None))
