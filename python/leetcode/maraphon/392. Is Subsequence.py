22c56259e6edc830d024f813cd2470e619e4f7c0
haxul <s.s.starodubov@yandex.ru>
2024-02-11 17:15:20 +0400

    algo

--- python/leetcode/maraphon/392. Is Subsequence.py
+++ python/leetcode/maraphon/392. Is Subsequence.py
@@ -1,2 +1,12 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
+        if not s:
+            return True
+        s_idx = 0
+        for i in range(len(t)):
+            if t[i] == s[s_idx]:
+                s_idx += 1
+            if s_idx == len(s):
+                return True
+
+        return False
