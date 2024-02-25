bdb5189397ce4236171cdd3a10642330a69fb7e2
haxul <s.s.starodubov@yandex.ru>
2024-02-25 14:05:03 +0400

    leetcode

--- kotlin/src/main/kotlin/Main.kt
+++ kotlin/src/main/kotlin/Main.kt
@@ -1,20 +1,4 @@
+
 fun main() {
-    var n = readln().toInt()
 
-    while (n != 1) {
-        print("$n ")
-        n = nextDev(n)
-    }
-
-    print(1)
-}
-
-fun nextDev(n:Int):Int  {
-   for (i in n- 1 downTo 0) {
-      if ( n % i == 0) {
-          return i
-      }
-   }
-
-   return 1
 }
