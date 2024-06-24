272ee69143a88a930caf201b8e742c05250ea126
starodubov <s.s.starodubov@yandex.ru>
2024-06-24 16:39:41 +0400

    leetcode

---
+++ python/leetcode/neetcode_all_course/1899. Merge Triplets to Form Target Triplet.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+       s =set()
+       for t in triplets:
+           if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
+               continue
+
+           for i , v in enumerate(t):
+               if v == target[i]:
+                   s.add(i)
+       return len(s) == 3
+
