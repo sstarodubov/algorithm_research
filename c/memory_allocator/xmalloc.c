c631cbf9c5cbe53040b5bb4862a9df334de1f4c1
starodubov <s.s.starodubov@yandex.ru>
2025-03-30 18:04:19 +0400

    sd

--- c/memory_allocator/xmalloc.c
+++ c/memory_allocator/xmalloc.c
@@ -1,56 +1,108 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #define HEAP_CAP 64000
-#define HEAP_ALLOCED_CAP 1024 
+#define CHUNK_LIST_CAP 1024 
 
-// 33.59
+// 1.23.56
 typedef struct {
     void* start;
     size_t size;
-} Heap_chunk;
+} Chunk;
 
+typedef struct {
+    size_t count;
+    Chunk chunks[CHUNK_LIST_CAP];
+} Chunk_List;
 
-Heap_chunk heap_alloced[] = {0};
+Chunk_List alloced_chunks = {0};
+Chunk_List freed_chunks = {0};
+
+Chunk heap_freed[CHUNK_LIST_CAP] = {0};
+Chunk heap_alloced[] = {0};
+
 char heap[HEAP_CAP] = {0};
-
 size_t heap_size = 0;
-size_t heap_alloced_size = 0;
+
+int compare(const void* a, const void* b) {
+    const Chunk* a_chunk = a;
+    const Chunk* b_chunk = b;
+    return a_chunk->start - b_chunk->start;
+}
+
+int chunk_list_find(const Chunk_List* list, void* ptr) {
+    Chunk key = {
+        .start = ptr
+    };
+    Chunk* result = bsearch(&key, list->chunks,list->count, sizeof(list->chunks[0]), compare);
+    if (result != NULL) {
+        assert(list->chunks <= result);
+        return result - list->chunks;
+    } else {
+        return -1;
+    }
+}
+
+void chunk_list_remove(Chunk_List* list, size_t idx) {
+    assert(idx < list->count);
+    for (size_t i = idx; i < list->count - 1; ++i) {
+        list->chunks[i] = list->chunks[i + 1];
+    }
+
+    list->count -= 1;
+}
+
+void chunk_list_insert(Chunk_List *list, void* ptr, int size) {
+    assert(list->count < CHUNK_LIST_CAP);
+    const int idx = list->count;
+    list->chunks[idx].start = ptr;
+    list->chunks[idx].size = size;
+    for (size_t i = list->count - 1; i > 0 && list->chunks[i].start < list->chunks[i - 1].start; --i) {
+        Chunk t = list->chunks[i];
+        list->chunks[i] = list->chunks[i - 1];
+        list->chunks[i - 1] = t;
+    }
+
+    list->count += 1;
+}
+
+void chunk_list_dump(const Chunk_List* list) {
+    printf("dump chunks: (%zu)\n", list->count);
+    for (int i = 0; i < list->count; i++) {
+        printf("idx: %d   start: %p, size: %zu\n",
+             i, list->chunks[i].start, list->chunks[i].size);
+    }
+}
+
 void* heap_alloc(size_t size) {
     if (size <=0 ) {
         return NULL;
     }
 
     assert(heap_size + size <= HEAP_CAP);
-    void* result = heap + heap_size;
+    void* ptr = heap + heap_size;
     heap_size += size;
-    assert(heap_alloced_size < HEAP_ALLOCED_CAP);
-    const Heap_chunk chunk = {
-        .start = result,
-        .size = size
-    };
 
-    heap_alloced[heap_alloced_size++] = chunk;
-    return result;
+    chunk_list_insert(&alloced_chunks, ptr, size);
+    return ptr;
 }
 
 void heap_free(void* ptr) {
-
-}
-
-void heap_dump_alloced_chunks(void) {
-    printf("Alloced chunks: (%zu)\n", heap_alloced_size);
-    for (int i = 0; i < heap_alloced_size; i++) {
-        printf("   start: %p, size: %zu\n", heap_alloced[i].start, heap_alloced[i].size);
-    }
+    const int idx = chunk_list_find(&alloced_chunks, ptr);
+    assert(idx >= 0);
+    chunk_list_insert(&freed_chunks, alloced_chunks.chunks[idx].start, alloced_chunks.chunks[idx].size);
+    chunk_list_remove(&alloced_chunks, (size_t) idx);
 }
 
 int main() {
     char *root = heap_alloc(25);
     char *root2 = heap_alloc(30);
+    char *root3 = heap_alloc(55);
 
-    heap_dump_alloced_chunks();
     heap_free(root);
+    chunk_list_dump(&alloced_chunks);
+    chunk_list_dump(&freed_chunks);
     return 0;
 }
